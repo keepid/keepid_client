d11f665731726a92086990e1080306711d5102eb
jalbi <jbigbee@seas.upenn.edu>
2019-12-05 12:52:00 -0500

    pdf stuff

--- src/main/PDFUpload/PDF_Mongo.java
+++ src/main/PDFUpload/PDF_Mongo.java
@@ -1,9 +1,8 @@
 package PDFUpload;
 
 import Config.MongoConfig;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
@@ -26,13 +25,11 @@ import java.util.Objects;
 
 class PDF_Mongo {
 
-    static ObjectId upload (String user, String title, String path){
+    static ObjectId upload (String user, String title, String path, MongoDatabase db) {
         System.out.println("Calling upload...");
-        MongoClient mongoClient = new MongoClient(new MongoClientURI(Objects.requireNonNull(Dotenv.load().get("MONGO_URI"))));
         ObjectId fileId = null;
         try {
-            MongoDatabase database = mongoClient.getDatabase(MongoConfig.getDatabaseName());
-            GridFSBucket gridBucket = GridFSBuckets.create(database);
+            GridFSBucket gridBucket = GridFSBuckets.create(db);
             InputStream inputStream = new FileInputStream(new File(path));
             GridFSUploadOptions options = new GridFSUploadOptions()
                     .chunkSizeBytes(1024)
@@ -44,20 +41,12 @@ class PDF_Mongo {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        finally {
-            mongoClient.close();
-        }
         return fileId;
-
-
     }
-    void download (String fileName){
+    void download (String fileName, MongoDatabase db){
         System.out.println("Calling download...");
-        MongoClient mongoClient = new MongoClient(new MongoClientURI(Objects.requireNonNull(Dotenv.load().get("MONGO_URI"))));
-
         try {
-            MongoDatabase database = mongoClient.getDatabase(MongoConfig.getDatabaseName());
-            GridFSBucket gridBucket = GridFSBuckets.create(database);
+            GridFSBucket gridBucket = GridFSBuckets.create(db);
 
             FileOutputStream fileOutputStream = new FileOutputStream("/download/" + fileName);
             gridBucket.downloadToStream(fileName, fileOutputStream);
@@ -65,10 +54,7 @@ class PDF_Mongo {
 
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            mongoClient.close();
         }
     }
+}
 
-}
-}
--- src/main/PDFUpload/PDF_upload.java
+++ src/main/PDFUpload/PDF_upload.java
@@ -1,17 +1,24 @@
 package PDFUpload;
 import java.io.File;
+
+import com.mongodb.client.MongoDatabase;
 import io.javalin.core.util.FileUtil;
 import io.javalin.http.Handler;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class PDF_upload
-{   public Handler pdf_upload = ctx -> {
+{
+    MongoDatabase db;
+    public PDF_upload(MongoDatabase db){
+        this.db = db;
+    }
+    public Handler pdf_upload = ctx -> {
         HttpServletRequest req = ctx.req;
         String username = req.getParameter("username");
         ctx.uploadedFiles("files").forEach(file -> {
             FileUtil.streamToFile(file.getContent(), "upload/" + username + file.getFilename());
-            PDF_Mongo.upload(username, file.getFilename(), "upload/" + username + file.getFilename());
+            PDF_Mongo.upload(username, file.getFilename(), "upload/" + username + file.getFilename(), this.db);
         });
     };
 }
