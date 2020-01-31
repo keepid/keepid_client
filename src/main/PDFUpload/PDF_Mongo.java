d2a4f0f725ff7fc9a11ec94794376131d163b9cb
jalbi <jbigbee@seas.upenn.edu>
2020-01-31 01:25:55 -0500

    upload hopefully works

--- src/main/PDFUpload/PDF_Mongo.java
+++ src/main/PDFUpload/PDF_Mongo.java
@@ -1,12 +1,14 @@
 package PDFUpload;
 
 import Config.MongoConfig;
+import com.mongodb.Block;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
 import com.mongodb.client.gridfs.GridFSUploadStream;
+import com.mongodb.client.gridfs.model.GridFSFile;
 import com.mongodb.client.gridfs.model.GridFSUploadOptions;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.bson.BSON;
@@ -31,12 +33,20 @@ class PDF_Mongo {
         try {
             GridFSBucket gridBucket = GridFSBuckets.create(db);
             InputStream inputStream = new FileInputStream(new File(path));
+            System.out.println("2 here");
             GridFSUploadOptions options = new GridFSUploadOptions()
                     .chunkSizeBytes(1024)
                     .metadata(new Document("type", "pdf")
                             .append("upload_date", String.valueOf(LocalDate.now()))
                             .append("uploader", user));
+            System.out.println("3 here");
             fileId = gridBucket.uploadFromStream(title, inputStream, options);
+            gridBucket.find().forEach(
+                    new Block<GridFSFile>() {
+                        public void apply(final GridFSFile gridFSFile) {
+                            System.out.println(gridFSFile.getFilename());
+                        }
+                    });
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
