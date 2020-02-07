d1cefdb797bf83215c9b172b84150df5d027e475
jalbi <jbigbee@seas.upenn.edu>
2020-02-07 13:07:18 -0500

    work work work work work

--- src/main/PDFUpload/PDF_Mongo.java
+++ src/main/PDFUpload/PDF_Mongo.java
@@ -1,14 +1,8 @@
 package PDFUpload;
 
-import com.mongodb.Block;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
-<<<<<<< HEAD
-import com.mongodb.client.gridfs.GridFSFindIterable;
-import com.mongodb.client.gridfs.GridFSUploadStream;
-=======
->>>>>>> aebdcc8d8bd97e74efa599226898af47bbc0cf3a
 import com.mongodb.client.gridfs.model.GridFSFile;
 import com.mongodb.client.gridfs.model.GridFSUploadOptions;
 import org.bson.Document;
@@ -16,46 +10,33 @@ import org.bson.types.ObjectId;
 
 import java.io.*;
 import java.time.LocalDate;
-<<<<<<< HEAD
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-=======
->>>>>>> aebdcc8d8bd97e74efa599226898af47bbc0cf3a
 
 public class PDF_Mongo {
 
   static ObjectId upload(String user, String title, String path, MongoDatabase db) {
-    System.out.println("Calling upload...");
-    ObjectId fileId = null;
-    try {
-      GridFSBucket gridBucket = GridFSBuckets.create(db);
-      InputStream inputStream = new FileInputStream(new File(path));
-      System.out.println("2 here");
-      GridFSUploadOptions options =
-          new GridFSUploadOptions()
-              .chunkSizeBytes(1024)
-              .metadata(
-                  new Document("type", "pdf")
-                      .append("upload_date", String.valueOf(LocalDate.now()))
-                      .append("uploader", user));
-      System.out.println("3 here");
-      fileId = gridBucket.uploadFromStream(title, inputStream, options);
-      gridBucket
-          .find()
-          .forEach(
-              new Block<GridFSFile>() {
-                public void apply(final GridFSFile gridFSFile) {
-                  System.out.println(gridFSFile.getFilename());
-                }
-              });
-
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-<<<<<<< HEAD
-    //Add option user
+      System.out.println("Calling upload...");
+      ObjectId fileId = null;
+      try {
+          GridFSBucket gridBucket = GridFSBuckets.create(db);
+          InputStream inputStream = new FileInputStream(new File(path));
+          System.out.println("2 here");
+          GridFSUploadOptions options =
+                  new GridFSUploadOptions()
+                          .chunkSizeBytes(1024)
+                          .metadata(
+                                  new Document("type", "pdf")
+                                          .append("upload_date", String.valueOf(LocalDate.now()))
+                                          .append("uploader", user));
+          System.out.println("3 here");
+          fileId = gridBucket.uploadFromStream(title, inputStream, options);
+          return fileId;
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      }
+      return fileId;
+  }  //Add option user
     public static List<String> getAllFiles(MongoDatabase db){
         List<String> files = new LinkedList<String>();
         try {
@@ -68,8 +49,9 @@ public class PDF_Mongo {
             return files;
         }
     }
+
     //Add option user
-    public static void download (String fileName, MongoDatabase db){
+    public static void download(String fileName, MongoDatabase db){
         System.out.println("Calling download...");
         try {
             GridFSBucket gridBucket = GridFSBuckets.create(db);
@@ -77,20 +59,6 @@ public class PDF_Mongo {
             FileOutputStream fileOutputStream = new FileOutputStream(fileName);
             gridBucket.downloadToStream(fileName, fileOutputStream);
             fileOutputStream.close();
-=======
-    return fileId;
-  }
-
-  void download(String fileName, MongoDatabase db) {
-    System.out.println("Calling download...");
-    try {
-      GridFSBucket gridBucket = GridFSBuckets.create(db);
-
-      FileOutputStream fileOutputStream = new FileOutputStream("/download/" + fileName);
-      gridBucket.downloadToStream(fileName, fileOutputStream);
-      fileOutputStream.close();
->>>>>>> aebdcc8d8bd97e74efa599226898af47bbc0cf3a
-
     } catch (Exception e) {
       e.printStackTrace();
     }
--- src/main/PDFUpload/PDF_upload.java
+++ src/main/PDFUpload/PDF_upload.java
@@ -18,21 +18,8 @@ public class PDF_upload {
   public Handler pdf_upload =
       ctx -> {
         HttpServletRequest req = ctx.req;
-<<<<<<< HEAD
         String username = "test";//req.getParameter("name")
         UploadedFile file = ctx.uploadedFile("file");
-=======
-        String username = "test"; // req.getParameter("name");
-        // Files.writeString(Paths.get("hello.pdf"), ctx.body());
-        System.out.println("We are here");
-        // System.out.println("This is a print statment" + ctx.body());
-        // String file2 = ctx.body();
-        UploadedFile file = ctx.uploadedFile("file");
-        System.out.println("now we here");
-        // UploadedFile file = ctx.uploadedFile("file");
-        // System.out.println("We here" + ctx.body());
-        // System.out.println(ctx.body().length());
->>>>>>> aebdcc8d8bd97e74efa599226898af47bbc0cf3a
         assert file != null;
         System.out.print(file.getContent());
         System.out.println("We got here, " + "upload/" + username + file.getFilename());
@@ -41,10 +28,5 @@ public class PDF_upload {
             PDF_Mongo.upload(
                 username, file.getFilename(), "upload/" + username + file.getFilename(), this.db);
         System.out.println("4 here" + fileId.toString());
-<<<<<<< HEAD
     };
-
-=======
-      };
->>>>>>> aebdcc8d8bd97e74efa599226898af47bbc0cf3a
 }
