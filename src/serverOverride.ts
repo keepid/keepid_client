5628c45ec9461b8152ad559e702e3603f8cd3d96
crchong1 <chong.connor@gmail.com>
2020-03-03 22:39:04 -0500

    fixed server override

--- src/serverOverride.ts
+++ src/serverOverride.ts
@@ -1,6 +1,6 @@
 const getServerURL = () : string => {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://keepid-server-staging.herokuapp.com/';
+    return 'https://keepid-server-staging.herokuapp.com';
   }
   return 'http://localhost:7000';
 };
