cd104ecbf1ccf33403a691f901c2403bea79a867
jalbi <jbigbee@seas.upenn.edu>
2020-05-01 16:32:26 -0400

    password reset stuff

--- src/main/UserIntTests/ChangePassword.java
+++ src/main/UserIntTests/ChangePassword.java
@@ -6,9 +6,12 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import com.mongodb.client.model.Filters;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import io.javalin.http.Handler;
+import io.jsonwebtoken.Claims;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -17,6 +20,7 @@ import org.json.JSONObject;
 import javax.servlet.http.HttpServletRequest;
 
 import java.security.SecureRandom;
+import java.util.Date;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -25,13 +29,13 @@ public class ChangePassword {
     public ChangePassword(MongoDatabase db) {
         this.db = db;
     }
-    public Handler resetPassword =
+    public Handler forgotPassword =
             ctx -> {
+                long ttMillis = 6000000;
                 JSONObject req = new JSONObject(ctx.body());
-                String username = ctx.sessionAttribute("username");
-                String newPass = RandomStringUtils.
-                        random(8, 65, 122, true, true, null, new SecureRandom());
+                String username = req.getString("username");
                 MongoCollection<Document> userCollection = db.getCollection("user");
+                MongoCollection<Document> linkCollection = db.getCollection("link");
                 Document user = userCollection.find(eq("username", username)).first();
                 if (user == null) {
                     ctx.result("user does not exist");
@@ -42,11 +46,16 @@ public class ChangePassword {
                         ctx.result("email not attached to this user");
                     }
                     else {
+                        String id = "";
+                        do {
+                            id = RandomStringUtils.
+                                    random(25, 48, 122, true, true, null, new SecureRandom());
+                        } while (linkCollection.find(eq("id", id)).first() != null);
+                        String link = CreateResetLink.createJWT(id, "KeepID", username, "PasswordReset", ttMillis);
                         EmailUtil.sendEmail("smtp.gmail.com", "587", "keepidtest@gmail.com",
                                 "Keep ID", "t3stPasw", email, "New Password",
-                                "Your new password: " + newPass + "\nTo maintain security, " +
-                                        "please change this password after logging in.");
-                        ChangePassword.change(username, newPass, "user.get(\"password\", String.class)", db);
+                                "https://keep.id/resetPassword/" + link
+                                );
                     }
 
                 }
@@ -66,6 +75,36 @@ public class ChangePassword {
                 }
                 ctx.json(res);
             };
+    public Handler resetPassword =
+            ctx -> {
+                JSONObject req = new JSONObject(ctx.body());
+                String jwt = ctx.pathParam("jwt");
+                Claims claim = CreateResetLink.decodeJWT(jwt);
+                System.out.println(claim);
+                //Check if everything is valid exp user id (maybe set up hash map of seen)
+                long nowMillis = System.currentTimeMillis();
+                Date now = new Date(nowMillis);
+                String id = claim.getId();
+                MongoCollection<Document> userCollection = db.getCollection("user");
+                MongoCollection<Document> resetIDs = db.getCollection("emailIDs");
+                Document user = userCollection.find(eq("username", claim.getAudience())).first();
+                System.out.println(user);
+                Document resetID = resetIDs.find(Filters.eq("id", id)).first();
+                System.out.println(resetID + "reset id");
+                JSONObject res = new JSONObject();
+                if (!(claim.getExpiration().compareTo(now) < 0 || user == null || resetID != null)){
+                    System.out.println("in");
+                    Document newID =
+                            new Document("id", id)
+                                    .append("expiration", claim.getExpiration());
+                    resetIDs.insertOne(newID);
+                    String newPassword = req.getString("newPassword");
+                    reset(claim.getAudience(), newPassword, db);
+                    res.put("status", "success");
+                    ctx.json(res);
+                }
+            };
+
     public static boolean change(String username, String newPassword, String oldPassword, MongoDatabase db) {
         Argon2 argon2 = Argon2Factory.create();
         MongoCollection<Document> userCollection = db.getCollection("user");
@@ -82,21 +121,24 @@ public class ChangePassword {
             argon2.wipeArray(newPasswordArr);
             return false;
         }
+        argon2.wipeArray(newPasswordArr);
+        reset(username, newPassword, db);
+        return true;
+    }
+    private static void reset(String username, String newPassword, MongoDatabase db) {
+        Argon2 argon2 = Argon2Factory.create();
+        MongoCollection<Document> userCollection = db.getCollection("user");
+        Document user = userCollection.find(eq("username", username)).first();
+        char[] newPasswordArr = newPassword.toCharArray();
         String passwordHash = argon2.hash(10, 65536, 1, newPasswordArr);
-
         Document query = new Document();
         query.append("_id",user.get("_id"));
+        System.out.println(user.get("_id") + " the id");
         Document setData = new Document();
         setData.append("password", passwordHash);
         Document update = new Document();
         update.append("$set", setData);
         userCollection.updateOne(query, update);
-
-
-        System.out.println(userCollection.find(eq("username", username)).first());
-        argon2.wipeArray(oldPasswordArr);
         argon2.wipeArray(newPasswordArr);
-        System.out.println(user.get("password", String.class));
-        return true;
     }
 }
