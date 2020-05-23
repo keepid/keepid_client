55f650f3987c66be2e25d1d16633b8e09cc39650
loafyyy <jackiepeng12@gmail.com>
2020-05-23 00:32:28 -0400

    add multiple pdf upload functionality

--- src/components/UploadDocs.tsx
+++ src/components/UploadDocs.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { withAlert } from 'react-alert';
 import DocumentViewer from './DocumentViewer';
 import getServerURL from '../serverOverride';
+
 
 interface Props {
   alert: any
@@ -11,7 +12,20 @@ interface Props {
 
 interface State {
   submitStatus: boolean,
-  pdfFile: File | undefined,
+  pdfFiles: FileList | undefined,
+}
+
+function RenderPDF(props) {
+  const [showResults, setShowResults] = useState(false);
+  return (
+    <li className="mt-3" key={props.index}>
+      <div className="row">
+        <button className="btn btn-outline-primary btn-sm mr-3" onClick={() => setShowResults(!showResults)}>{showResults ? 'Hide' : 'View'}</button>
+        <p>{props.pdfFile.name}</p>
+      </div>
+      { showResults ? <div className="row mt-3"><DocumentViewer pdfFile={props.pdfFile} /></div> : null }
+    </li>
+  );
 }
 
 class UploadDocs extends React.Component<Props, State> {
@@ -21,7 +35,7 @@ class UploadDocs extends React.Component<Props, State> {
     this.handleChangeFileUpload = this.handleChangeFileUpload.bind(this);
     this.state = {
       submitStatus: false,
-      pdfFile: undefined,
+      pdfFiles: undefined,
     };
     this.submitForm = this.submitForm.bind(this);
     this.handleChangeFileUpload = this.handleChangeFileUpload.bind(this);
@@ -30,49 +44,84 @@ class UploadDocs extends React.Component<Props, State> {
   submitForm(event: any) {
     event.preventDefault();
     const {
-      pdfFile,
+      pdfFiles,
     } = this.state;
-    if (pdfFile) {
-      const formData = new FormData();
-      formData.append('file', pdfFile, pdfFile.name);
-      fetch(`${getServerURL()}/upload`, {
-        method: 'POST',
-        credentials: 'include',
-        body: formData,
-      }).then((response) => response.json())
-        .then((responseJSON) => {
-          responseJSON = JSON.parse(responseJSON);
-          const {
-            status,
-          } = responseJSON;
-          if (status === 'success') {
-            this.props.alert.show('Successfully Uploaded File');
-            this.setState({
-              submitStatus: true,
-            });
-          } else {
-            this.props.alert.show('Failure to Upload File');
-          }
-        });
+    if (pdfFiles) {
+      // upload each pdf file
+      for (let i = 0; i < pdfFiles.length; i++) {
+        const pdfFile = pdfFiles[i];
+        const formData = new FormData();
+        formData.append('file', pdfFile, pdfFile.name);
+        fetch(`${getServerURL()}/upload`, {
+          method: 'POST',
+          credentials: 'include',
+          body: formData,
+        }).then((response) => response.json())
+          .then((responseJSON) => {
+            responseJSON = JSON.parse(responseJSON);
+            const {
+              status,
+            } = responseJSON;
+            if (status === 'success') {
+              this.props.alert.show(`Successfully uploaded ${pdfFile.name}`);
+              this.setState({
+                submitStatus: true,
+              });
+            } else {
+              this.props.alert.show(`Failure to upload ${pdfFile.name}`);
+            }
+          });
+      }
     } else {
       this.props.alert.show('Please select a file');
     }
   }
 
+  maxFilesExceeded(files, maxNumFiles) {
+    return files.length > maxNumFiles;
+  }
+
+  fileNamesUnique(files) {
+    const fileNames : string[] = [];
+    for (let i = 0; i < files.length; i++) {
+      const fileName = files[i].name;
+      fileNames.push(fileName);
+    }
+
+    return fileNames.length === new Set(fileNames).size;
+  }
+
   handleChangeFileUpload(event: any) {
     event.preventDefault();
-    const file : File = event.target.files[0];
-    this.setState({ pdfFile: file });
+    const { files } = event.target;
+    const maxNumFiles = 3;
+
+    // check that the number of files uploaded doesn't exceed the maximum
+    if (this.maxFilesExceeded(files, maxNumFiles)) {
+      event.target.value = null; // discard selected files
+      this.props.alert.show(`A maximum of ${maxNumFiles} files can be uploaded at a time`);
+      return;
+    }
+
+    // check that file names are unique
+    if (!this.fileNamesUnique(files)) {
+      event.target.value = null; // discard selected files
+      this.props.alert.show('File names must be unique');
+      return;
+    }
+
+    // all validation met
+    this.setState({
+      pdfFiles: files,
+    });
   }
 
   render() {
     const {
       submitStatus,
-      pdfFile,
+      pdfFiles,
     } = this.state;
-    if (submitStatus) {
-      return <Redirect to="/home" />;
-    }
+
     return (
       <div className="container">
         <Helmet>
@@ -87,25 +136,21 @@ class UploadDocs extends React.Component<Props, State> {
             After confirming that you have chosen the correct file, click the &quot;Upload&quot; button to upload.
             Otherwise, choose a different file.
           </p>
-          {pdfFile
-            ? (
-              <div className="row">
-                <DocumentViewer pdfFile={pdfFile} />
-              </div>
-            ) : <div />}
+
+          <ul className="list-unstyled mt-5">
+            {
+                pdfFiles && pdfFiles.length > 0 ? Array.from(pdfFiles).map((pdfFile, index) => <RenderPDF index={index} pdfFile={pdfFile} />) : null
+              }
+          </ul>
+
           <div className="row justify-content-left form-group mb-5">
             <form onSubmit={this.submitForm}>
               <div className="form-row mt-3">
-                <div className="col">
-                  <label className="btn btn-filestack btn-widget ml-5 mr-5">
-                    Choose File
-                    {' '}
-                    <input type="file" accept="application/pdf" id="potentialPdf" onChange={this.handleChangeFileUpload} hidden />
-                  </label>
-                </div>
-                <div className="col">
-                  <button type="submit" className="btn btn-outline-success">Upload</button>
-                </div>
+                <label className="btn btn-filestack btn-widget ml-5 mr-5">
+                  { pdfFiles && pdfFiles.length > 0 ? 'Choose New Files' : 'Choose Files' }
+                  <input type="file" accept="application/pdf" id="potentialPdf" multiple onChange={this.handleChangeFileUpload} hidden />
+                </label>
+                { pdfFiles && pdfFiles.length > 0 ? <button type="submit" className="btn btn-success">Upload</button> : null}
               </div>
             </form>
           </div>
