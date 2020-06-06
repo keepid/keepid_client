f5057eac5b4d03c34acaa8fea395d8e71eea8803
Connor Chong <chong.connor@gmail.com>
2020-06-06 04:20:08 -0500

    added styling

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,10 +1,6 @@
-// Idea: worker only access to client which permitted him, we can change who a client can edit
-// Superadmin can only do set admin
-
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-// import paginationFactory from 'react-bootstrap-table2-paginator';
 import { Helmet } from 'react-helmet';
 import Select from 'react-select';
 import { Link } from 'react-router-dom';
@@ -68,7 +64,6 @@ class AdminPanel extends Component<Props, State> {
         name: '',
         role: '',
       }],
-      // we should also pass in other state such as the admin information. we could also do a fetch call inside
     };
     this.onClickWorker = this.onClickWorker.bind(this);
     this.handleChangeSearchName = this.handleChangeSearchName.bind(this);
