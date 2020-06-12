e3c5d61bcaa1608905df13ad1674d8f6546b3559
loafyyy <jackiepeng12@gmail.com>
2020-06-12 14:55:25 -0400

    update google maps find organization page

---
+++ src/components/MapComponent.tsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
+import OrganizationMarker from './OrganizationMarker';
+
+interface Props {
+  organizations: any,
+  lat: number,
+  lng: number,
+}
+
+interface State {
+
+}
+
+class MapComponent extends Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      organizations,
+      lat,
+      lng
+    } = this.props;
+
+    return(
+      <GoogleMap
+        defaultZoom={12}
+        defaultCenter={{
+          lat: lat,
+          lng: lng,
+        }}
+      >
+        {organizations.map(
+          (organization, index) => (
+            <OrganizationMarker 
+              key={index}
+              lat={organization.lat}
+              lng={organization.lng}
+              orgName={organization.orgName}
+              address={organization.address}
+              phone={organization.phone}
+              email={organization.email}
+            />
+          ))
+        }
+      </GoogleMap>
+    )
+  }
+}
+
+export default withScriptjs(withGoogleMap(MapComponent))
