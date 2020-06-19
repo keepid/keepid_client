d36b8daf74a0d8056c37d3a8b6666183cb6a3b32
Gregory Kofman <kofmangregory@gmail.com>
2020-06-19 13:18:41 -0400

    Added a missing field

--- src/test/UserTest/UserControllerIntegrationTest.java
+++ src/test/UserTest/UserControllerIntegrationTest.java
@@ -56,6 +56,8 @@ public class UserControllerIntegrationTest {
     assertThat(actualResponseJSON.getString("lastName")).isEqualTo("");
     assert (actualResponseJSON.has("organization"));
     assertThat(actualResponseJSON.getString("organization")).isEqualTo("");
+    assert (actualResponseJSON.has("userRole"));
+    assertThat(actualResponseJSON.getString("userRole")).isEqualTo("");
     assert (actualResponseJSON.has("status"));
     assertThat(actualResponseJSON.getString("status")).isEqualTo("AUTH_FAILURE");
   }
