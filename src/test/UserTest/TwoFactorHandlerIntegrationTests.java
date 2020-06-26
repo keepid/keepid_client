4dfba845cf22c58069cda80410f6b28b9f1fee40
Daniel A Barychev <dbaryche@andrew.cmu.edu>
2020-06-25 21:04:01 -0400

    Change file name

--- src/test/UserTest/TwoFactorHandlerIntegrationTests.java
+++ src/test/UserTest/TwoFactorHandlerIntegrationTests.java
@@ -16,7 +16,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AccountSecurityIntegrationTests {
+public class TwoFactorHandlerIntegrationTests {
     private static Javalin app = Javalin.create();
 
     @BeforeClass
