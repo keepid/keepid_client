394bbc6ba6b4421c42bb4f2953bf24b85ca779bd
Steffen Cornwell <steffencornwell@gmail.com>
2020-07-03 00:04:35 -0400

    PDFApplication Unit Tests

--- src/main/PDF/PdfApplication.java
+++ src/main/PDF/PdfApplication.java
@@ -4,27 +4,16 @@ import User.UserType;
 import com.mongodb.client.MongoDatabase;
 import io.javalin.http.Handler;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
-import org.apache.pdfbox.pdmodel.interactive.form.PDField;
-import org.apache.pdfbox.pdmodel.interactive.form.PDNonTerminalField;
-import org.apache.pdfbox.pdmodel.interactive.form.PDTextField;
+import org.apache.pdfbox.pdmodel.interactive.form.*;
 import org.bson.types.ObjectId;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 public class PdfApplication {
 
@@ -34,13 +23,21 @@ public class PdfApplication {
     this.db = db;
   }
 
-  public static void main(String[] args) throws IOException {}
+  public static Set<String> validFieldTypes =
+      new HashSet<>(
+          Arrays.asList(
+              "CheckBox",
+              "PushButton",
+              "RadioButton",
+              "ComboBox",
+              "ListBox",
+              "TextField",
+              "SignatureField"));
 
   public Handler getApplicationQuestions =
       ctx -> {
         JSONObject req = new JSONObject(ctx.body());
         ObjectId applicationId = new ObjectId(req.getString("applicationId"));
-        // File pdfInput = new File("/home/steffen/Downloads/intellectual_property_release.pdf");
         String username = ctx.sessionAttribute("username");
         String organizationName = ctx.sessionAttribute("orgName");
         UserType privilegeLevel = ctx.sessionAttribute("privilegeLevel");
@@ -50,48 +47,77 @@ public class PdfApplication {
         PDDocument pdfDocument = PDDocument.load(inputStream);
         pdfDocument.setAllSecurityToBeRemoved(true);
 
-        List<String> fieldNames = new LinkedList<String>();
-        List<String> fieldQuestions = new LinkedList<String>();
-        PDAcroForm acroForm = pdfDocument.getDocumentCatalog().getAcroForm();
-        List<PDField> fields = acroForm.getFields();
-
-        while (!fields.isEmpty()) {
-          PDField field = fields.get(0);
-
-          // If the field has a child
-          if (field instanceof PDNonTerminalField) {
-            List<PDField> childrenFields = ((PDNonTerminalField) field).getChildren();
-            fields.addAll(childrenFields);
-          } else {
-            // fieldNames.add(getFieldFullName(field));
-            fieldNames.add(field.getFullyQualifiedName());
-            fieldQuestions.add("Please Enter Your " + field.getPartialName());
-          }
-
-          fields.remove(0);
-        }
-
-        for (String fieldName : fieldNames) {
-          System.out.println(fieldName);
-        }
-        for (String fieldQuestion : fieldQuestions) {
-          System.out.println(fieldQuestion);
-        }
-
+        List<JSONObject> fieldsJSON = new LinkedList<>();
+        getFieldInformation(pdfDocument, fieldsJSON);
         pdfDocument.close();
-        JSONObject res = new JSONObject();
-        res.put("fieldNames", fieldNames);
-        res.put("fieldQuestions", fieldQuestions);
-        ctx.json(res.toString());
+
+        ctx.json(fieldsJSON);
       };
 
-  private String getFieldFullName(PDField field) {
-    String fullName = field.getPartialName();
-    while (field.getParent() != null) {
-      field = field.getParent();
-      fullName = field.getPartialName() + "." + fullName;
+  /*
+   @Param fieldsJSON is an empty List of JSON, pdfDocument is the document
+  */
+  public static void getFieldInformation(PDDocument pdfDocument, List<JSONObject> fieldsJSON) {
+    PDAcroForm acroForm = pdfDocument.getDocumentCatalog().getAcroForm();
+    List<PDField> fields = acroForm.getFields();
+    while (!fields.isEmpty()) {
+      PDField field = fields.get(0);
+      if (field instanceof PDNonTerminalField) {
+        // If the field has children
+        List<PDField> childrenFields = ((PDNonTerminalField) field).getChildren();
+        fields.addAll(childrenFields);
+      } else {
+        JSONObject fieldJSON = new JSONObject();
+        String fieldType = "";
+        String fieldValueOptions = "[]";
+        if (field instanceof PDButton) {
+          if (field instanceof PDCheckBox) {
+            fieldType = "CheckBox";
+          } else if (field instanceof PDPushButton) {
+            fieldType = "PushButton";
+          } else if (field instanceof PDRadioButton) {
+            fieldType = "RadioButton";
+            PDRadioButton radioButtonField = (PDRadioButton) field;
+            JSONArray optionsJSONArray = new JSONArray();
+            for (String choice : radioButtonField.getOnValues()) {
+              optionsJSONArray.put(choice);
+            }
+            fieldValueOptions = optionsJSONArray.toString();
+          }
+        } else if (field instanceof PDVariableText) {
+          if (field instanceof PDChoice) {
+            if (field instanceof PDComboBox) {
+              fieldType = "ComboBox";
+            } else if (field instanceof PDListBox) {
+              fieldType = "ListBox";
+            }
+            PDChoice choiceField = (PDChoice) field;
+            JSONArray optionsJSONArray = new JSONArray();
+            for (String choice : choiceField.getOptions()) {
+              optionsJSONArray.put(choice);
+            }
+            fieldValueOptions = optionsJSONArray.toString();
+          } else if (field instanceof PDTextField) {
+            fieldType = "TextField";
+          }
+        } else if (field instanceof PDSignatureField) {
+          fieldType = "SignatureField";
+        }
+
+        // Not Editable
+        fieldJSON.put("fieldName", field.getFullyQualifiedName());
+        fieldJSON.put("fieldType", fieldType);
+        fieldJSON.put("fieldValueOptions", fieldValueOptions);
+
+        // Editable
+        fieldJSON.put("fieldQuestion", "Please Enter Your " + field.getPartialName());
+        fieldJSON.put("fieldMatchedDBVariable", "");
+        fieldJSON.put("fieldMatchedDBName", "");
+
+        fieldsJSON.add(fieldJSON);
+      }
+      fields.remove(0);
     }
-    return fullName;
   }
 
   public Handler fillPDFForm =
@@ -103,38 +129,18 @@ public class PdfApplication {
         UserType privilegeLevel = ctx.sessionAttribute("privilegeLevel");
         JSONObject formAnswers = req.getJSONObject("formAnswers");
 
-        // File pdfInput = new File("/home/steffen/Downloads/intellectual_property_release.pdf");
         InputStream inputStream =
             PdfMongo.download(
-                username,
-                organizationName,
-                privilegeLevel,
-                applicationId,
-                PDFType.APPLICATION.FORM,
-                db);
+                username, organizationName, privilegeLevel, applicationId, PDFType.FORM, db);
         PDDocument pdfDocument = PDDocument.load(inputStream);
         pdfDocument.setAllSecurityToBeRemoved(true);
 
-        // ImportXFDF importXFDFObject = new ImportXFDF();
-        // String xmlString = toXFDF(req);
-        // InputStream stream = new
-        // ByteArrayInputStream(xmlString.getBytes(StandardCharsets.UTF_8));
-        // FDFDocument xfdfDocument = FDFDocument.loadXFDF(stream);
-        // importXFDFObject.importFDF(pdfDocument, xfdfDocument);
-
-        PDAcroForm acroForm = pdfDocument.getDocumentCatalog().getAcroForm();
-        for (String key : formAnswers.keySet()) {
-          String fieldName = key; // + "." + key;
-          System.out.println(fieldName);
-          PDField field = acroForm.getField(fieldName);
-          if (field instanceof PDTextField) {
-            System.out.println(field.getPartialName());
-            String value = formAnswers.getString(key);
-            field.setValue(value);
-          }
+        try {
+          fillFields(pdfDocument, formAnswers);
+        } catch (IOException exception) {
+          ctx.result("failure");
         }
 
-        // pdfDocument.save("/home/steffen/Downloads/test.pdf");
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         pdfDocument.save(outputStream);
         pdfDocument.close();
@@ -143,6 +149,35 @@ public class PdfApplication {
         ctx.header("Content-Type", "application/pdf");
         ctx.result(outputFileStream);
       };
+
+  public static void fillFields(PDDocument pdfDocument, JSONObject formAnswers) throws IOException {
+    PDAcroForm acroForm = pdfDocument.getDocumentCatalog().getAcroForm();
+    for (String fieldName : formAnswers.keySet()) {
+      System.out.println(fieldName);
+      PDField field = acroForm.getField(fieldName);
+      if (field instanceof PDTextField) {
+        System.out.println(field.getPartialName());
+        String value = formAnswers.getString(fieldName);
+        field.setValue(value);
+      }
+    }
+  }
+
+  /*
+  private String getFieldFullName(PDField field) {
+    String fullName = field.getPartialName();
+    while (field.getParent() != null) {
+      field = field.getParent();
+      fullName = field.getPartialName() + "." + fullName;
+    }
+    return fullName;
+  }
+  // ImportXFDF importXFDFObject = new ImportXFDF();
+  // String xmlString = toXFDF(req);
+  // InputStream stream = new
+  // ByteArrayInputStream(xmlString.getBytes(StandardCharsets.UTF_8));
+  // FDFDocument xfdfDocument = FDFDocument.loadXFDF(stream);
+  // importXFDFObject.importFDF(pdfDocument, xfdfDocument);
 
   private String toXFDF(JSONObject req)
       throws ParserConfigurationException, TransformerConfigurationException, TransformerException {
@@ -179,4 +214,5 @@ public class PdfApplication {
     String xmlString = stringWriter.toString();
     return (xmlString);
   }
+   */
 }
---
+++ src/test/PDFTest/PDFApplicationUnitTests.java
@@ -0,0 +1,362 @@
+package PDFTest;
+
+import PDF.PdfApplication;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class PDFApplicationUnitTests {
+
+  // Get subset of fields of type fieldType
+  private List<JSONObject> getFieldsOfType(List<JSONObject> fieldsJSON, String fieldType) {
+    List<JSONObject> newFieldsJSON = new LinkedList<>();
+    for (JSONObject field : fieldsJSON) {
+      if (field.getString("fieldType").equals(fieldType)) {
+        newFieldsJSON.add(field);
+      }
+    }
+    return newFieldsJSON;
+  }
+
+  private JSONObject createFieldJSON(
+      String fieldName, String fieldType, String[] fieldValueOptions) {
+    JSONArray fieldValueOptionsJSON = new JSONArray();
+    for (String fieldValueOption : fieldValueOptions) {
+      fieldValueOptionsJSON.put(fieldValueOption);
+    }
+    return (new JSONObject()
+        .put("fieldName", fieldName)
+        .put("fieldType", fieldType)
+        .put("fieldValueOptions", fieldValueOptionsJSON.toString())
+        .put("fieldMatchedDBName", "")
+        .put("fieldMatchedDBVariable", "")
+        .put("fieldQuestion", "Please Enter Your " + fieldName));
+  }
+
+  class JSONFieldComparator implements Comparator<JSONObject> {
+    public int compare(JSONObject a, JSONObject b) {
+      return (a.getString("fieldName").compareTo(b.getString("fieldName")));
+    }
+  }
+
+  @Test
+  // NOTE: We need to fix the file so that the buttons are radio
+  public void getFieldInformationValidValuesTest1() throws IOException {
+    File pdfInput = new File("src/test/resources/testpdf.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Make sure field names are correct
+    for (JSONObject field : fieldsJSON) {
+      System.out.println(field.toString());
+
+      // Not Editable
+      Assert.assertNotEquals("", field.getString("fieldName"));
+      Assert.assertNotEquals("", field.getString("fieldType"));
+      Assert.assertNotEquals(null, field.getString("fieldValueOptions"));
+
+      // Editable
+      Assert.assertNotEquals("", field.getString("fieldQuestion"));
+      Assert.assertEquals("", field.getString("fieldMatchedDBName"));
+      Assert.assertEquals("", field.getString("fieldMatchedDBVariable"));
+
+      // Is valid fieldType
+      Set<String> validFieldTypes = PdfApplication.validFieldTypes;
+      Assert.assertTrue(validFieldTypes.contains(field.getString("fieldType")));
+    }
+  }
+
+  @Test
+  // NOTE: We need to fix the file so that the buttons are radio
+  public void getFieldInformationValidValuesTest2() throws IOException {
+    File pdfInput = new File("src/test/resources/ss-5.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Make sure field names are correct
+    for (JSONObject field : fieldsJSON) {
+      System.out.println(field.toString());
+
+      // Not Editable
+      Assert.assertNotEquals("", field.getString("fieldName"));
+      Assert.assertNotEquals("", field.getString("fieldType"));
+      Assert.assertNotEquals(null, field.getString("fieldValueOptions"));
+
+      // Editable
+      Assert.assertNotEquals("", field.getString("fieldQuestion"));
+      Assert.assertEquals("", field.getString("fieldMatchedDBName"));
+      Assert.assertEquals("", field.getString("fieldMatchedDBVariable"));
+
+      // Is valid fieldType
+      Set<String> validFieldTypes = PdfApplication.validFieldTypes;
+      Assert.assertTrue(validFieldTypes.contains(field.getString("fieldType")));
+    }
+  }
+
+  @Test
+  // NOTE: We need to fix the file so that the buttons are radio
+  public void getFieldInformationValidValuesTest3() throws IOException {
+    File pdfInput = new File("src/test/resources/Application_for_a_Birth_Certificate.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Make sure field names are correct
+    for (JSONObject field : fieldsJSON) {
+      System.out.println(field.toString());
+
+      // Not Editable
+      Assert.assertNotEquals("", field.getString("fieldName"));
+      Assert.assertNotEquals("", field.getString("fieldType"));
+      Assert.assertNotEquals(null, field.getString("fieldValueOptions"));
+
+      // Editable
+      Assert.assertNotEquals("", field.getString("fieldQuestion"));
+      Assert.assertEquals("", field.getString("fieldMatchedDBName"));
+      Assert.assertEquals("", field.getString("fieldMatchedDBVariable"));
+
+      // Is valid fieldType
+      Set<String> validFieldTypes = PdfApplication.validFieldTypes;
+      Assert.assertTrue(validFieldTypes.contains(field.getString("fieldType")));
+    }
+  }
+
+  @Test
+  public void getFieldInformationTextFieldTest() throws IOException {
+    File pdfInput = new File("src/test/resources/testpdf.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Set up correct field names and questions
+    List<JSONObject> correctFieldsJSON = new LinkedList<>();
+    String[] fieldValueOptions = {};
+    correctFieldsJSON.add(createFieldJSON("Name", "TextField", fieldValueOptions));
+    correctFieldsJSON.add(createFieldJSON("Title", "TextField", fieldValueOptions));
+    correctFieldsJSON.add(createFieldJSON("Address", "TextField", fieldValueOptions));
+    correctFieldsJSON.add(createFieldJSON("A different address", "TextField", fieldValueOptions));
+    correctFieldsJSON.add(createFieldJSON("currentdate_af_date", "TextField", fieldValueOptions));
+    correctFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> correctFieldIterator = correctFieldsJSON.iterator();
+
+    // Get Only Fields Needed for This Test
+    List<JSONObject> testFieldsJSON = getFieldsOfType(fieldsJSON, "TextField");
+    testFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> testFieldsJSONIterator = testFieldsJSON.iterator();
+
+    // Check Not Editable Fields are correct
+    while (testFieldsJSONIterator.hasNext() || correctFieldIterator.hasNext()) {
+      JSONObject field = testFieldsJSONIterator.next();
+      JSONObject correctField = correctFieldIterator.next();
+
+      // Change to equals for GSON
+      Assert.assertEquals(correctField.getString("fieldName"), field.getString("fieldName"));
+      Assert.assertEquals(correctField.getString("fieldType"), field.getString("fieldType"));
+      Assert.assertEquals(
+          correctField.getString("fieldValueOptions"), field.getString("fieldValueOptions"));
+    }
+  }
+
+  @Test
+  public void getFieldInformationComboBoxTest() throws IOException {
+    File pdfInput = new File("src/test/resources/testpdf.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Set up correct field names and questions
+    List<JSONObject> correctFieldsJSON = new LinkedList<>();
+    String[] fieldValueOptions = {"Choice1", "Choice2", "Choice3"};
+    correctFieldsJSON.add(createFieldJSON("Dropdown", "ComboBox", fieldValueOptions));
+    correctFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> correctFieldIterator = correctFieldsJSON.iterator();
+
+    // Get Only Fields Needed for This Test
+    List<JSONObject> testFieldsJSON = getFieldsOfType(fieldsJSON, "ComboBox");
+    testFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> testFieldsJSONIterator = testFieldsJSON.iterator();
+
+    // Check Not Editable Fields are correct
+    while (testFieldsJSONIterator.hasNext() || correctFieldIterator.hasNext()) {
+      JSONObject field = testFieldsJSONIterator.next();
+      JSONObject correctField = correctFieldIterator.next();
+
+      // Change to equals for GSON
+      Assert.assertEquals(correctField.getString("fieldName"), field.getString("fieldName"));
+      Assert.assertEquals(correctField.getString("fieldType"), field.getString("fieldType"));
+      Assert.assertEquals(
+          correctField.getString("fieldValueOptions"), field.getString("fieldValueOptions"));
+    }
+  }
+
+  @Test
+  public void getFieldInformationListBoxTest() throws IOException {
+    File pdfInput = new File("src/test/resources/testpdf.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Set up correct field names and questions
+    List<JSONObject> correctFieldsJSON = new LinkedList<>();
+    String[] fieldValueOptions = {"Choice1", "Choice2", "Choice3"};
+    correctFieldsJSON.add(createFieldJSON("Combobox", "ListBox", fieldValueOptions));
+    correctFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> correctFieldIterator = correctFieldsJSON.iterator();
+
+    // Get Only Fields Needed for This Test
+    List<JSONObject> testFieldsJSON = getFieldsOfType(fieldsJSON, "ListBox");
+    testFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> testFieldsJSONIterator = testFieldsJSON.iterator();
+
+    // Check Not Editable Fields are correct
+    while (testFieldsJSONIterator.hasNext() || correctFieldIterator.hasNext()) {
+      JSONObject field = testFieldsJSONIterator.next();
+      JSONObject correctField = correctFieldIterator.next();
+
+      // Change to equals for GSON
+      Assert.assertEquals(correctField.getString("fieldName"), field.getString("fieldName"));
+      Assert.assertEquals(correctField.getString("fieldType"), field.getString("fieldType"));
+      Assert.assertEquals(
+          correctField.getString("fieldValueOptions"), field.getString("fieldValueOptions"));
+    }
+  }
+
+  @Test
+  public void getFieldInformationCheckBoxTest() throws IOException {
+    File pdfInput = new File("src/test/resources/testpdf.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Set up correct field names and questions
+    List<JSONObject> correctFieldsJSON = new LinkedList<>();
+    String[] fieldValueOptions = {};
+    correctFieldsJSON.add(createFieldJSON("Chicken", "CheckBox", fieldValueOptions));
+    correctFieldsJSON.add(createFieldJSON("Vegetables", "CheckBox", fieldValueOptions));
+    correctFieldsJSON.add(createFieldJSON("Ribeye Steaks", "CheckBox", fieldValueOptions));
+    correctFieldsJSON.add(createFieldJSON("Tomatoes", "CheckBox", fieldValueOptions));
+    correctFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> correctFieldIterator = correctFieldsJSON.iterator();
+
+    // Get Only Fields Needed for This Test
+    List<JSONObject> testFieldsJSON = getFieldsOfType(fieldsJSON, "CheckBox");
+    testFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> testFieldsJSONIterator = testFieldsJSON.iterator();
+
+    // Check Not Editable Fields are correct
+    while (testFieldsJSONIterator.hasNext() || correctFieldIterator.hasNext()) {
+      JSONObject field = testFieldsJSONIterator.next();
+      JSONObject correctField = correctFieldIterator.next();
+
+      // Change to equals for GSON
+      Assert.assertEquals(correctField.getString("fieldName"), field.getString("fieldName"));
+      Assert.assertEquals(correctField.getString("fieldType"), field.getString("fieldType"));
+      Assert.assertEquals(
+          correctField.getString("fieldValueOptions"), field.getString("fieldValueOptions"));
+    }
+  }
+
+  @Test
+  public void getFieldInformationPushButtonTest() throws IOException {
+    File pdfInput = new File("src/test/resources/testpdf.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Set up correct field names and questions
+    List<JSONObject> correctFieldsJSON = new LinkedList<>();
+    String[] fieldValueOptions = {};
+    correctFieldsJSON.add(createFieldJSON("Submit", "PushButton", fieldValueOptions));
+    correctFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> correctFieldIterator = correctFieldsJSON.iterator();
+
+    // Get Only Fields Needed for This Test
+    List<JSONObject> testFieldsJSON = getFieldsOfType(fieldsJSON, "PushButton");
+    testFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> testFieldsJSONIterator = testFieldsJSON.iterator();
+
+    // Check Not Editable Fields are correct
+    while (testFieldsJSONIterator.hasNext() || correctFieldIterator.hasNext()) {
+      JSONObject field = testFieldsJSONIterator.next();
+      JSONObject correctField = correctFieldIterator.next();
+
+      // Change to equals for GSON
+      Assert.assertEquals(correctField.getString("fieldName"), field.getString("fieldName"));
+      Assert.assertEquals(correctField.getString("fieldType"), field.getString("fieldType"));
+      Assert.assertEquals(
+          correctField.getString("fieldValueOptions"), field.getString("fieldValueOptions"));
+    }
+  }
+
+  @Test
+  public void getFieldInformationRadioButtonTest() throws IOException {
+    File pdfInput = new File("src/test/resources/testpdf.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Set up correct field names and questions
+    List<JSONObject> correctFieldsJSON = new LinkedList<>();
+    String[] fieldValueOptions = {"Yes", "No", "Maybe"};
+    correctFieldsJSON.add(createFieldJSON("Radiobuttons", "RadioButton", fieldValueOptions));
+    correctFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> correctFieldIterator = correctFieldsJSON.iterator();
+
+    // Get Only Fields Needed for This Test
+    List<JSONObject> testFieldsJSON = getFieldsOfType(fieldsJSON, "RadioButton");
+    testFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> testFieldsJSONIterator = testFieldsJSON.iterator();
+
+    // Check Not Editable Fields are correct
+    while (testFieldsJSONIterator.hasNext() || correctFieldIterator.hasNext()) {
+      JSONObject field = testFieldsJSONIterator.next();
+      JSONObject correctField = correctFieldIterator.next();
+
+      // Change to equals for GSON
+      Assert.assertEquals(correctField.getString("fieldName"), field.getString("fieldName"));
+      Assert.assertEquals(correctField.getString("fieldType"), field.getString("fieldType"));
+      Assert.assertEquals(
+          correctField.getString("fieldValueOptions"), field.getString("fieldValueOptions"));
+    }
+  }
+
+  @Test
+  public void getFieldInformationSignatureFieldTest() throws IOException {
+    File pdfInput = new File("src/test/resources/testpdf.pdf");
+    PDDocument pdfDocument = PDDocument.load(pdfInput);
+    List<JSONObject> fieldsJSON = new LinkedList<>();
+    PdfApplication.getFieldInformation(pdfDocument, fieldsJSON);
+
+    // Set up correct field names and questions
+    List<JSONObject> correctFieldsJSON = new LinkedList<>();
+    String[] fieldValueOptions = {};
+    correctFieldsJSON.add(createFieldJSON("Signature", "SignatureField", fieldValueOptions));
+    correctFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> correctFieldIterator = correctFieldsJSON.iterator();
+
+    // Get Only Fields Needed for This Test
+    List<JSONObject> testFieldsJSON = getFieldsOfType(fieldsJSON, "SignatureField");
+    testFieldsJSON.sort(new JSONFieldComparator());
+    Iterator<JSONObject> testFieldsJSONIterator = testFieldsJSON.iterator();
+
+    // Check Not Editable Fields are correct
+    while (testFieldsJSONIterator.hasNext() || correctFieldIterator.hasNext()) {
+      JSONObject field = testFieldsJSONIterator.next();
+      JSONObject correctField = correctFieldIterator.next();
+
+      // Change to equals for GSON
+      Assert.assertEquals(correctField.getString("fieldName"), field.getString("fieldName"));
+      Assert.assertEquals(correctField.getString("fieldType"), field.getString("fieldType"));
+      Assert.assertEquals(
+          correctField.getString("fieldValueOptions"), field.getString("fieldValueOptions"));
+    }
+  }
+}
