076a0e6ae1dce8951d20b04968d1ae85b912df81
Melinda Cardenas <mc6377@nyu.edu>
2020-08-04 19:54:20 -0400

    created skeleton for my organization page

---
+++ src/components/AccountSecurity/MyOrganization.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+interface Props {}
+
+interface State {}
+
+class MyOrganization extends Component<Props, State>{
+	constructor(props: Props) {
+		super(props);
+	}
+	render(){
+	return(
+		<h1>hello world</h1>
+	);
+}}
+
+export default MyOrganization;
