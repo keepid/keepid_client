8313a6ccee0f1b4be1c9a52154e525090e048431
Connor Chong <chong.connor@gmail.com>
2020-08-21 11:42:01 -0500

    reenabled cors

--- src/main/Config/AppConfig.java
+++ src/main/Config/AppConfig.java
@@ -121,7 +121,7 @@ public class AppConfig {
               config.defaultContentType =
                       "text/plain"; // content type to use if no content type is set (default is
               // "text/plain")
-//              config.enableCorsForAllOrigins(); // enable cors for all origins
+              config.enableCorsForAllOrigins(); // enable cors for all origins
 //              config.enableDevLogging(); // enable extensive development logging for http and
               // websocket
               config.enforceSsl =
