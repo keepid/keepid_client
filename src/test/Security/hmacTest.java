e03955c0027d1b1141d0414da3ad0fe747eb76ac
Austin Wu <avwu99@gmail.com>
2020-08-22 14:28:49 -0500

    Deleted Hmac Test

--- src/test/Security/hmacTest.java
+++ src/test/Security/hmacTest.java
@@ -1,14 +1,3 @@
 package Security;
 
-import org.junit.Test;
-
-import java.security.GeneralSecurityException;
-
-public class hmacTest {
-
-  @Test
-  public void hashString() throws GeneralSecurityException {
-    SecurityUtils securityUtils = new SecurityUtils();
-    System.out.println(securityUtils.hashString("Hello World"));
-  }
-}
+public class hmacTest {}
