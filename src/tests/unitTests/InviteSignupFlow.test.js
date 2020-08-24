19b53ac18a22ba7089b1390a4f934f46fc45b218
Melinda Cardenas <mc6377@nyu.edu>
2020-08-24 16:58:46 -0400

    changed to date object for unit tests

--- src/tests/unitTests/InviteSignupFlow.test.js
+++ src/tests/unitTests/InviteSignupFlow.test.js
@@ -1,7 +1,11 @@
 import {birthDateStringConverter} from "../../components/SignUp/InviteSignupFlow";
 
+let dateTest1 = new Date(1998,1,16);
+let dateTest2 = new Date(1990,2,6);
+let dateTest3 = new Date(98,5,12);
+
 test('birth date string converter test',() => {
-	expect(birthDateStringConverter("01-16-1998")).toBe("01-16-1998");
-	expect(birthDateStringConverter("2-6-1998")).toBe("02-06-1998");
-	expect(birthDateStringConverter("5-12-80")).toBe("05-12-1980");
+	expect(birthDateStringConverter(dateTest1)).toBe("01-16-1998");
+	expect(birthDateStringConverter(dateTest2)).toBe("02-06-1998");
+	expect(birthDateStringConverter(dateTest3)).toBe("05-12-1980");
 })
