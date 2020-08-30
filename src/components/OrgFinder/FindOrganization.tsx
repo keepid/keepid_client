010c08d681f65b1dc134b5cbb65f2f70a3c7ce3d
Joey Zhao <joeyzhao.zhao26@gmail.com>
2020-08-30 19:39:48 -0400

    distance function

--- src/components/OrgFinder/FindOrganization.tsx
+++ src/components/OrgFinder/FindOrganization.tsx
@@ -14,7 +14,10 @@ interface State {
   displayError: boolean,
   displayIcon: boolean,
   zipcodeSearch: string,
-  zipcodeLatLng: any,
+  zipcodeLat: any,
+  zipcodeLng: any,
+  zipcodeLatLang: any,
+  results: any,
 }
 
 const APIKey = 'AIzaSyBS1seMnrtdwOxpcoezbN_QVwVp797Dxyw';
@@ -28,7 +31,10 @@ class FindOrganization extends Component<Props, State> {
       displayError: false,
       displayIcon: true,
       zipcodeSearch: '',
-      zipcodeLatLng: {},
+      zipcodeLat: {},
+      zipcodeLng: {},
+      zipcodeLatLang: {},
+      results: [],
     };
     this.getOrganizations = this.getOrganizations.bind(this);
     this.geocodeZipcode = this.geocodeZipcode.bind(this);
@@ -41,6 +47,7 @@ class FindOrganization extends Component<Props, State> {
   }
 
   getOrganizations() {
+    const count = 0;
     const organizations = [
       {
         orgName: 'Broad Street Ministries',
@@ -49,6 +56,7 @@ class FindOrganization extends Component<Props, State> {
         address: '315 S Broad St, Philadelphia, PA 19107',
         phone: '',
         email: '',
+        count,
       },
     ];
     this.setState({ organizations });
@@ -66,6 +74,7 @@ class FindOrganization extends Component<Props, State> {
   geocodeZipcode() {
     const {
       zipcodeSearch,
+      results,
     } = this.state;
     const url = new URL('https://maps.googleapis.com/maps/api/geocode/json?'); // Do in OrganizationSignup
     const search = `postal_code:${zipcodeSearch}`;
@@ -80,13 +89,38 @@ class FindOrganization extends Component<Props, State> {
       .then((responseJSON) => {
         const { status } = responseJSON;
 
+        // if valid zipcode
         if (status === 'OK') {
           const zipcodeLatLng = responseJSON.results[0].geometry.location;
+          const zipcodeLat = responseJSON.results[0].geometry.location.lat;
+          const zipcodeLng = responseJSON.results[0].geometry.location.lng;
+
+          // if within radius, post results
+          // this.withinDistance(zipcodeLat, zipcodeLng);
+          // there are results
+          // if (!results.isEmpty) {
+          //   this.setState({
+          //     displayMap: true,
+          //     displayIcon: false,
+          //     displayError: false,
+          //     zipcodeLat,
+          //     zipcodeLng,
+          //   });
+          // } else { // there are no results
+          //   this.setState({
+          //     displayMap: true,
+          //     displayIcon: false,
+          //     displayError: false,
+          //     zipcodeLat,
+          //     zipcodeLng,
+          //   });
+          // }
           this.setState({
             displayMap: true,
             displayIcon: false,
             displayError: false,
-            zipcodeLatLng,
+            zipcodeLat,
+            zipcodeLng,
           });
         } else {
           // const {
@@ -102,6 +136,25 @@ class FindOrganization extends Component<Props, State> {
       });
   }
 
+  withinDistance(zipcodeLat, zipcodeLng) {
+    const {
+      organizations,
+      results,
+    } = this.state;
+    // convert lat and lng to kilometers
+    const searchLatKilo = zipcodeLat / 110.574;
+    const searchLngKilo = zipcodeLng / (111.320 * Math.cos(searchLatKilo * Math.PI / 180));
+    for (let i = 0; i < organizations.length; i++) {
+      const orgLatKilo = organizations[i].lat / 110.574;
+      const orgLngKilo = organizations[i].lng / (111.320 * Math.cos(orgLatKilo * Math.PI / 180));
+      // check if org is within 10 miles
+      if ((Math.abs(searchLatKilo - orgLatKilo) < 10000)
+          && (Math.abs(searchLngKilo - orgLngKilo) < 10000)) {
+        results.add(organizations[i]);
+      }
+    }
+  }
+
   render() {
     const {
       displayMap,
@@ -109,23 +162,24 @@ class FindOrganization extends Component<Props, State> {
       displayIcon,
       zipcodeSearch,
       organizations,
-      zipcodeLatLng,
+      zipcodeLat,
+      zipcodeLng,
     } = this.state;
 
     return (
       <div className="container">
-        
+
         <div className="jumbotron-fluid mt-5">
           <h1 className="text-center"><b>Find Organizations Near You</b></h1>
         </div>
         <form onSubmit={this.onSubmitZipcode}>
           <div className="input-group w-50 mb-3 mt-5 mx-auto">
-            <input 
-              type="text" 
-              className="form-control form-purple" 
-              placeholder="Search by zipcode..." 
-              value={zipcodeSearch} 
-              onChange={this.onHandleChangeZipcode} 
+            <input
+              type="text"
+              className="form-control form-purple"
+              placeholder="Search by zipcode..."
+              value={zipcodeSearch}
+              onChange={this.onHandleChangeZipcode}
             />
             <div className="input-group-append">
               <button className="btn btn-info btn-primary-theme" type="submit">Search</button>
@@ -140,112 +194,122 @@ class FindOrganization extends Component<Props, State> {
             </div>
           )
           : <div />}
- 
-        <div className="row">
-          <div className="col-sm-6">
+
+        {displayMap
+          ? (
             <div className="row">
-              <h5 className="pb-3 mr-1 ml-3">3</h5>
-              <h5 className="pb-3">results near 19104</h5>
-            </div>
+              <div className="col-sm-6">
+                <div className="row">
+                  <h5 className="pb-3 mr-1 ml-3">3</h5>
+                  <h5 className="pb-3">results near 19104</h5>
+                </div>
 
-            <div className="row mx-md-n5">
-              <div className="shadow p-3 mb-5 ml-5 bg-white rounded">
-                <div className="col px-md-2">
-                  <h6 className="font-weight-bold">Broad Street Ministry</h6>
+                <div className="row mx-md-n5">
+                  <div className="shadow p-3 mb-5 ml-5 bg-white rounded">
+                    <div className="col px-md-2">
+                      <h6 className="font-weight-bold">Broad Street Ministry</h6>
+                    </div>
+                    <div className="col px-md-2"><b>315 S Broad St, Philadelphia, PA 19107</b></div>
+                    <div className="row px-md-4">
+                      <div className="text-green mr-1">
+                        Open
+                      </div>
+                      • Closes 4:00PM
+                    </div>
+                    <div className="row px-md-4 pb-4">
+                      Website:
+                      <a href="broadstreetministry.org" className="text-primary-theme ml-1">
+                        broadstreetministry.org
+                      </a>
+                    </div>
+                    <div className="row px-md-4">
+                      Call:
+                      <div className="text-primary-theme ml-1 mr-5">(215) 735-4847</div>
+                      <div className="ml-5 mr-1">Email:</div>
+                      <div className="text-primary-theme">broadstreetministry@gmail.com</div>
+                    </div>
                   </div>
-                <div className="col px-md-2"><b>315 S Broad St, Philadelphia, PA 19107</b></div>
-                <div className="row px-md-4">
-                  <div className="text-green mr-1">
-                    Open 
+                </div>
+
+                <div className="row mx-md-n5">
+                  <div className="shadow p-3 mb-5 ml-5 bg-white rounded">
+                    <div className="col px-md-2">
+                      <h6 className="font-weight-bold">Project Home</h6>
+                    </div>
+                    <div className="col px-md-2"><b>1515 Fairmont Ave, Philadelphia, PA 19130</b></div>
+                    <div className="row px-md-4">
+                      <div className="text-green mr-1">
+                        Open
+                      </div>
+                      • Closes 4:00PM
+                    </div>
+                    <div className="row px-md-4 pb-4">
+                      Website:
+                      <a href="projecthome.org" className="text-primary-theme ml-1">
+                        projecthome.org
+                      </a>
+                    </div>
+                    <div className="row px-md-4">
+                      Call:
+                      <div className="text-primary-theme ml-1 mr-5">(215) 232-7272</div>
+                      <div className="ml-5 mr-1">Email:</div>
+                      <div className="text-primary-theme mr-5">projecthome@gmail.com</div>
+                    </div>
                   </div>
-                  • Closes 4:00PM
                 </div>
-                <div className="row px-md-4 pb-4">Website:
-                    <a href="broadstreetministry.org" className="text-primary-theme ml-1">
-                    broadstreetministry.org
-                    </a>
-                </div>
-                <div className="row px-md-4">Call:
-                  <div className="text-primary-theme ml-1 mr-5">(215) 735-4847</div>
-                  <div className="ml-5 mr-1">Email:</div>
-                  <div className="text-primary-theme">broadstreetministry@gmail.com</div>
+
+                <div className="row mx-md-n5">
+                  <div className="shadow p-3 mb-5 ml-5 bg-white rounded">
+                    <div className="col px-md-2">
+                      <h6 className="font-weight-bold">Bethesda Project</h6>
+                    </div>
+                    <div className="col px-md-2"><b>1630 South Street, Philadelphia, PA 19146</b></div>
+                    <div className="row px-md-4">
+                      <div className="text-red mr-1">
+                        Closed
+                      </div>
+                      • Opens 9:00AM Monday
+                    </div>
+                    <div className="row px-md-4 pb-4">
+                      Website:
+                      <a href="bethesdaproject.org" className="text-primary-theme ml-1">
+                        bethesdaproject.org
+                      </a>
+                    </div>
+                    <div className="row px-md-4">
+                      Call:
+                      <div className="text-primary-theme ml-1 mr-5">(215) 985-1600</div>
+                      <div className="ml-5 mr-1">Email:</div>
+                      <div className="text-primary-theme mr-5">info@bethesdaproject.org</div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="row mx-md-n5">
-              <div className="shadow p-3 mb-5 ml-5 bg-white rounded">
-                <div className="col px-md-2">
-                  <h6 className="font-weight-bold">Project Home</h6>
-                  </div>
-                <div className="col px-md-2"><b>1515 Fairmont Ave, Philadelphia, PA 19130</b></div>
-                <div className="row px-md-4">
-                  <div className="text-green mr-1">
-                    Open 
-                  </div>
-                  • Closes 4:00PM
-                </div>
-                <div className="row px-md-4 pb-4">Website:
-                    <a href="projecthome.org" className="text-primary-theme ml-1">
-                    projecthome.org
-                    </a>
-                </div>
-                <div className="row px-md-4">Call:
-                  <div className="text-primary-theme ml-1 mr-5">(215) 232-7272</div>
-                  <div className="ml-5 mr-1">Email:</div>
-                  <div className="text-primary-theme mr-5">projecthome@gmail.com</div>
-                </div>
+              <div className="col mt-5">
+                {displayMap
+                  ? (
+                    <MapComponent
+                      organizations={organizations}
+                      lat={zipcodeLat}
+                      lng={zipcodeLng}
+                      googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${APIKey}&v=3.exp&libraries=geometry,drawing,places`}
+                      loadingElement={<div style={{ width: '100%', height: '100%' }} />}
+                      containerElement={<div style={{ height: '400px' }} />}
+                      mapElement={<div style={{ width: '100%', height: '100%' }} />}
+                    />
+                  )
+                  : <div />}
               </div>
             </div>
-
-            <div className="row mx-md-n5">
-              <div className="shadow p-3 mb-5 ml-5 bg-white rounded">
-                <div className="col px-md-2">
-                  <h6 className="font-weight-bold">Bethesda Project</h6>
-                  </div>
-                <div className="col px-md-2"><b>1630 South Street, Philadelphia, PA 19146</b></div>
-                <div className="row px-md-4">
-                  <div className="text-red mr-1">
-                    Closed 
-                  </div>
-                  • Opens 9:00AM Monday
-                </div>
-                <div className="row px-md-4 pb-4">Website:
-                    <a href="bethesdaproject.org" className="text-primary-theme ml-1">
-                    bethesdaproject.org
-                    </a>
-                </div>
-                <div className="row px-md-4">Call:
-                  <div className="text-primary-theme ml-1 mr-5">(215) 985-1600</div>
-                  <div className="ml-5 mr-1">Email:</div>
-                  <div className="text-primary-theme mr-5">info@bethesdaproject.org</div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col mt-5">    
-            {displayMap
-              ? (
-                <MapComponent
-                  organizations={organizations}
-                  lat={zipcodeLatLng.lat}
-                  lng={zipcodeLatLng.lng}
-                  googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${APIKey}&v=3.exp&libraries=geometry,drawing,places`}
-                  loadingElement={<div style={{ width: '100%', height: '100%'}} />}
-                  containerElement={<div style={{ height: '400px' }} />}
-                  mapElement={<div style={{ width: '100%', height: '100%' }} />}
-                />
-              )
-              : <div />}
-          </div>    
-        </div>
+          )
+          : <div />}
 
         {displayError
           ? (
             <div className="text-center">
               <p className="text-red">No results found. Make sure you are searching for a valid zipcode.</p>
-              <br></br>
+              <br />
               <img src={InvalidZipcodeIcon} className="img-fluid" alt="Guy looking into void" />
             </div>
           )
