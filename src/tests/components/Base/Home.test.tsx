17f7c51b72388c43367ea590d9dcc0e3941db279
crchong1 <chong.connor@gmail.com>
2021-01-20 21:11:27 -0500

    updating keepid

--- src/tests/components/Base/Home.test.tsx
+++ src/tests/components/Base/Home.test.tsx
@@ -5,12 +5,15 @@ import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import Home from '../../../components/Base/Home';
 
+window.alert = jest.fn();
 test('Home page loads', () => {
+  window.alert.mockClear();
   const { getByText } = render(<MemoryRouter><Home /></MemoryRouter>);
   getByText('Safeguarding identities of those experiencing homelessness');
 });
 
 test('Home page buttons work', () => {
+  window.alert.mockClear();
   const { getByText } = render(<MemoryRouter><Home /></MemoryRouter>);
   const button = getByText('Get Started');
   userEvent.click(button);
@@ -19,6 +22,7 @@ test('Home page buttons work', () => {
 });
 
 test('Home page snapshot test', () => {
+  window.alert.mockClear();
   const tree = renderer.create(<MemoryRouter><Home /></MemoryRouter>).toJSON();
   expect(tree).toMatchSnapshot();
 });
