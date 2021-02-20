f5c7ca96449907ce520469495fcd0d31ebd680e9
Gagandeep Kang <gskang@bu.edu>
2021-02-20 16:12:38 -0500

    add test for birth date string converter fn

--- src/tests/components/SignUp/CompleteSignupFlow.test.tsx
+++ src/tests/components/SignUp/CompleteSignupFlow.test.tsx
@@ -1,7 +1,17 @@
-import { addHttp } from '../../../components/SignUp/CompleteSignupFlow';
+import React from 'react';
+
+import { addHttp, birthDateStringConverter } from '../../../components/SignUp/CompleteSignupFlow';
 
 test('add http test', () => {
   expect(addHttp('https://example.com')).toBe('https://example.com');
   expect(addHttp('https://www.example.com')).toBe('https://www.example.com');
   expect(addHttp('www.example.org/somethinghere')).toBe('http://www.example.org/somethinghere');
+});
+
+describe('test birth date string converter function', () => {
+  test('JavaScript Date Object should turn into mm-dd-yyyy equivalent', () => {
+    const firstDate = new Date('February 19, 2021');
+    const formattedDate = birthDateStringConverter(firstDate);
+    expect(formattedDate).toBe('02-19-2021');
+  });
 });
