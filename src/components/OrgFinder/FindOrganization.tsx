c5bd5e3ddb892df984a6a33eb2d4022e7566b793
daniel joo <dsj2122@columbia.edu>
2021-04-27 01:58:57 -0700

    tried new api key 1

--- src/components/OrgFinder/FindOrganization.tsx
+++ src/components/OrgFinder/FindOrganization.tsx
@@ -19,7 +19,7 @@ interface State {
   searchLoading: boolean,
 }
 
-const APIKey = 'AIzaSyBS1seMnrtdwOxpcoezbN_QVwVp797Dxyw';
+const APIKey = 'AIzaSyA4vdAgDjHcX0fVWh6I8IQ-Yy1FQ_Be8wc';
 
 Geocode.setApiKey(APIKey);
 Geocode.setLanguage('en');
@@ -179,6 +179,7 @@ class FindOrganization extends Component<Props, State> {
       method: 'GET',
     }).then((response) => response.json())
       .then((responseJSON) => {
+        console.log(responseJSON);
         const { status } = responseJSON;
 
         if (status === 'OK') {
