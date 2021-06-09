5c4a56659da46e23f87b247f7c2d7a970225dc67
crchong1 <chong.connor@gmail.com>
2021-06-08 19:42:54 -1000

    updating snapshots and home test

--- src/tests/components/Base/Home.test.tsx
+++ src/tests/components/Base/Home.test.tsx
@@ -5,23 +5,43 @@ import { IntlProvider } from 'react-intl';
 import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
-import Home from '../../../components/Home';
+import Home from '../../../components/Home/index';
 
 window.scroll = jest.fn();
 test('Home page loads', () => {
-  const { getByText } = render(<MemoryRouter><IntlProvider locale="en" defaultLocale="en"><Home /></IntlProvider></MemoryRouter>);
-  getByText('Safeguarding identities of those experiencing homelessness');
+  const { getByText } = render(
+    <MemoryRouter>
+      <IntlProvider locale="en" defaultLocale="en">
+        <Home />
+      </IntlProvider>
+    </MemoryRouter>
+  );
+  getByText('An identification platform');
 });
 
 test('Home page buttons work', () => {
-  const { getByText } = render(<MemoryRouter><IntlProvider locale="en" defaultLocale="en"><Home /></IntlProvider></MemoryRouter>);
+  const { getByText } = render(
+    <MemoryRouter>
+      <IntlProvider locale="en" defaultLocale="en">
+        <Home />
+      </IntlProvider>
+    </MemoryRouter>
+  );
   const button = getByText('Get Started');
   userEvent.click(button);
-  const button2 = getByText('Learn More');
+  const button2 = getByText('Donate');
   userEvent.click(button2);
 });
 
 test('Home page snapshot test', () => {
-  const tree = renderer.create(<MemoryRouter><IntlProvider locale="en" defaultLocale="en"><Home /></IntlProvider></MemoryRouter>).toJSON();
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <IntlProvider locale="en" defaultLocale="en">
+          <Home />
+        </IntlProvider>
+      </MemoryRouter>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
