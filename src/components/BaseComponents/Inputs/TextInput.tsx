d64601a10cf53ef27f6b36facc5b682b98ec2968
nick rodriguez <nick@catalytic.com>
2021-12-05 19:31:22 -0600

    feat(text-input): support view/hide password

--- src/components/BaseComponents/Inputs/TextInput.tsx
+++ src/components/BaseComponents/Inputs/TextInput.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
+import { InputGroup } from 'react-bootstrap';
 
 import InputProps from './BaseInputProps';
 import InputWrapper from './InputWrapper';
@@ -7,11 +8,11 @@ import InputWrapper from './InputWrapper';
 export enum TextInputType {
   EMAIL = 'email',
   PASSWORD = 'password',
-  TEXT = 'text'
+  TEXT = 'text',
 }
 
 export interface TextInputProps extends InputProps<string> {
-  type?: TextInputType
+  type?: TextInputType;
 }
 
 const TextInput = ({
@@ -26,19 +27,34 @@ const TextInput = ({
   ...rest
 }: TextInputProps) => {
   const className = classNames('form-control', 'form-purple', classNameProp);
+  const [showPassword, setShowPassword] = useState(false);
+  const inputType =
+    type === TextInputType.PASSWORD && showPassword ? TextInputType.TEXT : type;
   return (
     <InputWrapper label={label} name={name} {...rest}>
-      <input
-        className={className}
-        defaultValue={defaultValue}
-        id={name}
-        name={name}
-        onChange={(e) => onChange && onChange(e.target.value)}
-        placeholder={placeholder}
-        required={rest.required}
-        type={type}
-        value={value}
-      />
+      <InputGroup>
+        <input
+          className={className}
+          defaultValue={defaultValue}
+          id={name}
+          name={name}
+          onChange={(e) => onChange && onChange(e.target.value)}
+          placeholder={placeholder}
+          required={rest.required}
+          type={inputType}
+          value={value}
+        />
+        {type === TextInputType.PASSWORD ? (
+          <InputGroup.Append>
+            <InputGroup.Text>
+              <i
+                onClick={() => setShowPassword(!showPassword)}
+                className={showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'}
+              />
+            </InputGroup.Text>
+          </InputGroup.Append>
+        ) : null}
+      </InputGroup>
     </InputWrapper>
   );
 };
