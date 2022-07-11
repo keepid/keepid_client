8c1ea58c162274dbb7e7a8e37a9d41e777b471d2
Olivia <ocao@princeton.edu>
2022-07-11 12:13:16 -0400

    convert PDF to text in EULA.tsx

--- src/components/SignUp/pages/SignUserAgreement.tsx
+++ src/components/SignUp/pages/SignUserAgreement.tsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet';
 import { defineMessages, useIntl } from 'react-intl';
 
 import SignaturePad from '../../../lib/SignaturePad';
-import EULA from '../../../static/EULA.pdf';
+import EULA from '../../AboutUs/EULA';
 import SignUpContext from '../SignUp.context';
 
 const messages = defineMessages({
@@ -36,57 +36,53 @@ export default function SignUserAgreement() {
   const [canvasDataUrl, setCanvasDataUrl] = useState('');
 
   return (
-    <div>
-      <Helmet>
-        <title>{intl.formatMessage(messages.title)}</title>
-        <meta name="description" content="Keep.id" />
-      </Helmet>
-      <div className="d-flex justify-content-center pt-5">
-        <div className="col-md-12">
-          <div className="text-center pb-4 mb-2">
-            <h2>
-              <b>{intl.formatMessage(messages.header)}</b>
-            </h2>
-          </div>
-          <div className="embed-responsive embed-responsive-16by9">
-            <iframe
-              className="embed-responsive-item"
-              src={EULA}
-              title="EULA Agreement"
-            />
-          </div>
-          <div className="d-flex justify-content-center pt-5">
-            <div className="col-md-8">
-              <div className="pb-3">
-                {intl.formatMessage(messages.agreeToTerms)}
-              </div>
-              <SignaturePad
-                acceptEULA={hasSigned}
-                handleChangeAcceptEULA={setHasSigned}
-                handleCanvasSign={setCanvasDataUrl}
-                canvasDataUrl={canvasDataUrl}
-              />
+        <div>
+            <Helmet>
+                <title>{intl.formatMessage(messages.title)}</title>
+                <meta name="description" content="Keep.id" />
+            </Helmet>
+            <div className="d-flex justify-content-center pt-5">
+                <div className="col-md-12">
+                    <div className="text-center pb-4">
+                        <h2>
+                            <b>{intl.formatMessage(messages.header)}</b>
+                        </h2>
+                    </div>
+                    <div className="col-md-12">
+                        <EULA />
+                    </div>
+                    <div className="d-flex justify-content-center pt-5">
+                        <div className="col-md-8">
+                            <div className="pb-3">
+                                {intl.formatMessage(messages.agreeToTerms)}
+                            </div>
+                            <SignaturePad
+                              acceptEULA={hasSigned}
+                              handleChangeAcceptEULA={setHasSigned}
+                              handleCanvasSign={setCanvasDataUrl}
+                              canvasDataUrl={canvasDataUrl}
+                            />
+                        </div>
+                    </div>
+                    <div className="d-flex">
+                        <button
+                          type="button"
+                          className="btn btn-outline-primary mt-5"
+                          onClick={moveToPreviousSignupStage}
+                        >
+                            Previous Step
+                        </button>
+                        <button
+                          type="button"
+                          className="ml-auto btn btn-primary mt-5"
+                          onClick={moveToNextSignupStage}
+                          disabled={!hasSigned}
+                        >
+                            Continue
+                        </button>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="d-flex">
-            <button
-              type="button"
-              className="btn btn-outline-primary mt-5"
-              onClick={moveToPreviousSignupStage}
-            >
-              Previous Step
-            </button>
-            <button
-              type="button"
-              className="ml-auto btn btn-primary mt-5"
-              onClick={moveToNextSignupStage}
-              disabled={!hasSigned}
-            >
-              Continue
-            </button>
-          </div>
         </div>
-      </div>
-    </div>
   );
 }
