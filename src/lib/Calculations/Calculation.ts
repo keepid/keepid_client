5f6b4845c7bcf48ebd9e99c070459972c3761c02
Olivia <ocao@princeton.edu>
2022-07-27 10:41:29 -0400

    write 2 functions to calculate age from birthday and add age to profile page

---
+++ src/lib/Calculations/Calculation.ts
@@ -0,0 +1,29 @@
+let calculateAge = (birthday: Date): number => {
+    const today = new Date();
+    let age = today.getFullYear() - birthday.getFullYear();
+    if (today.getMonth() <= birthday.getMonth()) {
+        if (today.getDay() < birthday.getDay()) {
+            age--;
+        }
+    }
+    return age;
+}
+
+let calculateAgeFromString = (birthday: string): number => {
+    const today = new Date();
+    const dateArr = birthday.split('-');
+    const year = parseInt(dateArr[2]);
+    const month = parseInt(dateArr[0]);
+    const day = parseInt(dateArr[1]);
+    let age = today.getFullYear() - year;
+    if (today.getMonth() <= month) {
+        if (today.getDay() < day) {
+            age--;
+        }
+    }
+    return age;
+}
+
+export {
+    calculateAge, calculateAgeFromString
+}
