ac9c30c9e6e504beea4d7e8a26ed101ccfb4b74a
steffen12 <steffencornwell@gmail.com>
2023-03-04 22:57:57 -0500

    Fix Application Current Date Format

--- src/components/Applications/ApplicationForm.tsx
+++ src/components/Applications/ApplicationForm.tsx
@@ -261,10 +261,10 @@ class ApplicationForm extends Component<Props, State> {
     const mm = date.getMonth() + 1; // getMonth() is zero-based
     const dd = date.getDate();
     const dateString = [
-      date.getFullYear(),
       (mm > 9 ? '' : '0') + mm,
       (dd > 9 ? '' : '0') + dd,
-    ].join('-');
+      date.getFullYear(),
+    ].join('/');
     return dateString;
   };
 
