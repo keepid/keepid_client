2e564c04f553809a048b95eadab13ff1f0be474b
Daniel Joo <dsj2122@columbia.edu>
2023-08-02 14:00:23 -0400

    Edited mail modal text

--- src/components/Documents/MailModal.tsx
+++ src/components/Documents/MailModal.tsx
@@ -20,7 +20,7 @@ export const MailModal: React.FC<Props> = ({ isVisible, setIsVisible, mailForm }
                 <Dialog.Panel className="tw-m-8 tw-w-[50rem] tw-rounded-md tw-bg-white tw-p-1 tw-px-4 tw-shadow-md">
                 <Dialog.Title className="tw-p-4 tw-text-left tw-font-body tw-text-lg">Send application by direct mail?</Dialog.Title>
                 <p className="tw-p-2 tw-text-left tw-font-body tw-text-sm">
-                    Agreeing to this action will send the application to Team Keep to print and directly mail to the corresponding agency. Your status will be notified in your &quot;My Documents&quot; Page.
+                    Agreeing to this action will send the application to Team Keep to print and directly mail to the corresponding agency. Your status will be notified through email.
                 </p>
                 <div className="tw-m-8 tw-mt-10 tw-grid tw-grid-flow-row-dense tw-grid-cols-2 tw-gap-20 sm:tw-gap-60">
                     <LoadingButton onClick={mailForm}>Yes, mail </LoadingButton>
