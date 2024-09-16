2d505d1b04d0eba0d5c843d7ff688f34e5ae09c9
crchong1 <chong.connor@gmail.com>
2024-09-16 19:41:37 -0400

    fixing server override

--- src/serverOverride.ts
+++ src/serverOverride.ts
@@ -3,7 +3,7 @@ const getServerURL = (): string => {
     return 'https://server.keep.id';
   }
   if (process.env.NODE_ENV === 'staging') {
-    return 'https://keepid-server-staging.herokuapp.com/';
+    return 'https://keepid-server-staging.herokuapp.com';
   }
   return 'http://localhost:7001';
 };
