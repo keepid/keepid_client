3b46d7f31b2c80fb5001ab635671afcf60a492f1
mgporter <26415062+mgporter@users.noreply.github.com>
2024-11-04 16:37:55 -0500

    linked SelectApplication page to Application page, modified App.tsx with link.

--- src/components/Applications/SelectApplication.tsx
+++ src/components/Applications/SelectApplication.tsx
@@ -3,11 +3,7 @@ import { Link, Route, Switch } from 'react-router-dom';
 
 function SelectApplication() {
   return (
-    <Switch>
-      <Route exact path="/applications/createnew">
-        <h1>Hello!</h1>
-      </Route>
-    </Switch>
+    <h1>Select Application here</h1>
   );
 }
 
