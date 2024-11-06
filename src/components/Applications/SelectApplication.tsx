33272f8fe6d134f22430f5e6aa4ea9805412e97f
mgporter <26415062+mgporter@users.noreply.github.com>
2024-11-06 13:45:03 -0500

    created initial SelectApplicationCard

--- src/components/Applications/SelectApplication.tsx
+++ src/components/Applications/SelectApplication.tsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 import { Link, Route, Switch } from 'react-router-dom';
 
 function SelectApplication() {
   return (
-    <h1>Select Application here</h1>
+    <div className="container-fluid">
+      <Helmet>
+        <title>Create new application</title>
+        <meta name="description" content="Keep.id" />
+      </Helmet>
+      <div className="jumbotron jumbotron-fluid bg-white pb-0">
+        <div className="container">
+          <h1>Select Application here</h1>
+        </div>
+      </div>
+    </div>
   );
 }
 
