0c406f07eb5774d86aafdc1cbada758759d37af5
mgporter <26415062+mgporter@users.noreply.github.com>
2024-11-08 22:28:54 -0500

    Created select application card provider and initial form

--- src/components/Applications/SelectApplicationCard.tsx
+++ src/components/Applications/SelectApplicationCard.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
+import { Button, ToggleButton } from 'react-bootstrap';
 
 interface SelectApplicationCardProps {
   iconSrc: string;
   iconAlt: string;
   titleText: string;
-  subtitleText: string;
+  subtitleText: string | null;
   clickHandler: () => void;
+  name: string;
+  checked: boolean;
+  disabled: boolean;
+  value: string | number;
 }
 
 export default function SelectApplicationCard({
   iconSrc,
   iconAlt,
   titleText,
-  subtitleText,
+  subtitleText = null,
   clickHandler,
+  name,
+  checked = false,
+  disabled = false,
+  value,
 }: SelectApplicationCardProps) {
   return (
-    <div onClick={clickHandler} className="tw-border-2 tw-border-gray-500 tw-size-64 tw-flex tw-flex-col tw-items-center">
-      <img alt={iconAlt} src={iconSrc} className="tw-w-3/4 tw-max-h-12" />
-      <p className="tw-text-lg tw-font-bold">{titleText}</p>
-      <p className="tw-text-md">{subtitleText}</p>
-    </div>
+    <ToggleButton
+      type="radio"
+      name={name}
+      checked={checked}
+      disabled={disabled}
+      id={`${name}-${value}`}
+      value={value}
+      onChange={clickHandler}
+      className="tw-border-2 tw-border-gray-500 tw-h-52 tw-w-56 tw-flex tw-flex-col tw-items-center
+        tw-bg-transparent tw-text-black tw-rounded-lg"
+    >
+      <img alt={iconAlt} src={iconSrc} className="tw-mb-4 tw-mt-8 tw-h-16 tw-w-auto tw-aspect-auto" />
+      <p className="tw-text-base tw-font-bold">{titleText}</p>
+      {subtitleText && <p className="tw-text-md">{subtitleText}</p>}
+    </ToggleButton>
   );
 }
