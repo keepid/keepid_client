014eae2365ac95f547a1d8ae21fb449bd3ffc8c5
mgporter <26415062+mgporter@users.noreply.github.com>
2024-11-13 00:36:46 -0500

    added document preview and mail page to form

--- src/components/Applications/SelectApplicationForm.tsx
+++ src/components/Applications/SelectApplicationForm.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, Component, FormEvent } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 
+import DocumentViewer from '../Documents/DocumentViewer';
 import useNewApplicationFormContext from './NewApplicationFormHook';
 import { ApplicationType } from './NewApplicationFormProvider';
 import SelectApplicationCard from './SelectApplicationCard';
@@ -24,11 +25,14 @@ export default function SelectApplicationForm() {
 
   const handleNext = () => setPage((prev) => prev + 1);
 
-  const questionPageCount = Object.keys(formContent).length;
+  const pageCount = Object.keys(formContent).length;
 
-  const disablePrev = page === 0;
+  const hidePrev = page === 0;
 
-  const disableNext = page === questionPageCount - 1;
+  const disableNext = page === 6;
+
+  const isPreviewPage = page === pageCount - 2;
+  const isFinalPage = page === pageCount - 1;
 
   return (
     <div className="container-fluid">
@@ -40,14 +44,15 @@ export default function SelectApplicationForm() {
         <div className="container tw-flex tw-flex-col tw-gap-4">
           <div className="tw-flex tw-justify-between tw-items-end">
             <h1>{formContent[page].title}</h1>
-            <p>Step {page + 1} of {questionPageCount}</p>
+            <p>Step {page + 1} of {pageCount}</p>
           </div>
 
-          {page === 3 && <h3>I am filling out this application on behalf of...</h3>}
+          {formContent[page].subtitle && <h3>{formContent[page].subtitle}</h3>}
 
           <Form className="form tw-flex tw-flex-wrap tw-gap-8 tw-justify-center" onSubmit={handleSubmit}>
 
-            {formContent[page].options
+            {formContent[page].dataAttr &&
+            formContent[page].options
               .filter((option) => option.for === null || option.for.has(data.type as ApplicationType))
               .map((option) => (
                 <SelectApplicationCard
@@ -56,17 +61,34 @@ export default function SelectApplicationForm() {
                   iconAlt={option.iconAlt}
                   titleText={option.titleText}
                   subtitleText={option.subtitleText}
-                  clickHandler={() => setData((prev) => ({ ...prev, [option.dataAttr]: option.value }))}
-                  checked={data[option.dataAttr] === option.value}
-                  name={option.dataAttr}
+                  clickHandler={() => setData((prev) => ({ ...prev, [formContent[page].dataAttr!]: option.value }))}
+                  checked={data[formContent[page].dataAttr!] === option.value}
+                  name={formContent[page].dataAttr!}
                   value={option.value}
                   disabled={false}
                 />
               ))}
 
           </Form>
+
+          {isPreviewPage && (
+            <DocumentViewer pdfFile={new File([new Blob()], 'test')} />
+          )}
+
+          {isFinalPage && (
+            <div>
+              <h5>This is the final page</h5>
+              <ul>
+                <li>{data.type}</li>
+                <li>{data.state}</li>
+                <li>{data.situation}</li>
+                <li>{data.person}</li>
+              </ul>
+            </div>
+          )}
+
           <div className="tw-flex tw-justify-between">
-            <Button onClick={handlePrev} className={`${disablePrev ? 'tw-invisible ' : ' '}`}>Back</Button>
+            <Button onClick={handlePrev} className={`${hidePrev ? 'tw-invisible ' : ' '}`}>Back</Button>
             <Button onClick={handleNext} className={`${disableNext ? 'tw-invisible ' : ' '}`}>Next</Button>
           </div>
         </div>
