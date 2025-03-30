d342f81a9bf2d009785d533ecdfe7f2119840a21
Tim Liang <timliang4@gmail.com>
2025-03-30 05:57:10 -0400

    change google sign in authentication flow to Sign in with Google for Web https://developers.google.com/identity/gsi/web/guides/overview

--- src/components/UserAuthentication/GoogleLoginButton.tsx
+++ src/components/UserAuthentication/GoogleLoginButton.tsx
@@ -1,44 +1,15 @@
 import { CredentialResponse, GoogleLogin, GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
 import React from 'react';
 
-import { generateCodeChallenge, generateCodeVerifier, generateState } from '../../pkceUtils';
+import getServerURL from '../../serverOverride';
 
-export const CLIENT_ID: string = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
-export const REDIRECT_URI = 'http://localhost:3000/callback';
-
-export default function GoogleLoginButton() {
-  const handleLoginSuccess = async (res: CredentialResponse) => {
-    const codeVerifier = generateCodeVerifier();
-    const codeChallenge = await generateCodeChallenge(codeVerifier);
-    const state = generateState();
-
-    console.log(res);
-
-    // Store code verifier and state in sessionStorage
-    sessionStorage.setItem('code_verifier', codeVerifier);
-    sessionStorage.setItem('oauth_state', state);
-
-    const params = new URLSearchParams({
-      client_id: CLIENT_ID,
-      redirect_uri: REDIRECT_URI,
-      response_type: 'code',
-      scope: 'openid email profile',
-      code_challenge: codeChallenge,
-      code_challenge_method: 'S256',
-      state, // CSRF protection
-      prompt: 'login', // Force re-authentication
-    });
-
-    // window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`;
-  };
-
+export default function GoogleLoginButton({ handleGoogleLoginSuccess }) {
   return (
       <GoogleLogin
-        onSuccess={handleLoginSuccess}
+        onSuccess={handleGoogleLoginSuccess}
         onError={() => console.error('Login Failed')}
         text="signin_with"
         theme="outline"
-        // prompt="login"
       />
   );
 }
