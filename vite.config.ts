45e2050d964a779afee30e39f44377f173c170c6
crchong1 <chong.connor@gmail.com>
2025-06-08 18:34:55 -0400

    fixing vite circleci pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 // vite.config.js
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import react from '@vitejs/plugin-react';
 import rollupNodePolyFill from 'rollup-plugin-polyfill-node'; // ✅ ADD THIS
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
