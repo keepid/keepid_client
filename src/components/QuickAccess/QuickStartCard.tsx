8bf6172e6bcd90309b9380196694d47c8778e950
Tim Liang <timliang4@gmail.com>
2025-10-04 00:35:33 -0400

    iconify quickstart card options and add back button

--- src/components/QuickAccess/QuickStartCard.tsx
+++ src/components/QuickAccess/QuickStartCard.tsx
@@ -1,7 +1,9 @@
-import { CheckCircleIcon } from '@heroicons/react/20/solid';
 import React, { useEffect, useState } from 'react';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 import getServerURL from '../../serverOverride';
+import ApplyIDIcon from '../../static/images/QuickAccess/ApplyID.svg';
+import UploadIDIcon from '../../static/images/QuickAccess/UploadID.svg';
 
 enum UserSituation {
   None = 'none',
@@ -21,6 +23,7 @@ const QuickStartCard = () => {
   const [minimized, setMinimized] = useState<boolean>(false);
   const [userSituation, setUserSituation] = useState<UserSituation>(UserSituation.None);
   const [checklistData, setChecklistData] = useState<ChecklistTask[]>([]);
+  const [checkedSituation, setCheckedSituation] = useState<UserSituation>(UserSituation.None);
 
   const fetchQuickStartChecklist = async () => {
     try {
@@ -54,12 +57,7 @@ const QuickStartCard = () => {
     fetchQuickStartChecklist();
   }, []);
 
-  const handleSituationSubmit = async (e) => {
-    e.preventDefault();
-    const selectedRadio : HTMLInputElement | null = document.querySelector('input[name="situation"]:checked');
-    const radioVal = selectedRadio?.value;
-    if (!radioVal) return;
-
+  const updateUserSituation = async (newUserSituation) => {
     try {
       const response = await fetch(`${getServerURL()}/onboarding-checklist`, {
         method: 'POST',
@@ -68,7 +66,7 @@ const QuickStartCard = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          situation: radioVal,
+          situation: newUserSituation,
           minimized: false,
         }),
       });
@@ -77,12 +75,26 @@ const QuickStartCard = () => {
         throw new Error(`Failed to submit situation: ${response.status}`);
       }
 
-      setUserSituation(radioVal as UserSituation);
+      setUserSituation(newUserSituation as UserSituation);
 
       await fetchQuickStartChecklist();
     } catch (error) {
       console.error('Error submitting situation:', error);
     }
+  };
+
+  const handleSituationSubmit = async (e) => {
+    e.preventDefault();
+    const selectedRadio : HTMLInputElement | null = document.querySelector('input[name="situation"]:checked');
+    const radioVal = selectedRadio?.value;
+    if (!radioVal) return;
+    updateUserSituation(radioVal);
+  };
+
+  const handleBack = async (e) => {
+    e.preventDefault();
+    setCheckedSituation(UserSituation.None);
+    updateUserSituation('none');
   };
 
   const handleMinimize = async () => {
@@ -140,12 +152,14 @@ const QuickStartCard = () => {
       {
         id: UserSituation.ApplyID,
         label: 'I want to apply for an ID',
-        description: 'I don\'t have a government ID and would like to apply for one',
+        iconAlt: `${UserSituation.ApplyID}-icon`,
+        iconSrc: ApplyIDIcon,
       },
       {
         id: UserSituation.UploadID,
         label: 'I have an ID to upload',
-        description: 'I have a government ID and want to upload it',
+        iconAlt: `${UserSituation.UploadID}-icon`,
+        iconSrc: UploadIDIcon,
       },
     ];
 
@@ -174,26 +188,48 @@ const QuickStartCard = () => {
             </p>
             <div>
               <div className="tw-space-y-3 tw-mb-4">
-                {situationOptions.map((option) => (
-                  <div key={option.id} className="tw-relative">
-                    <label className="tw-flex tw-items-start tw-cursor-pointer tw-p-3 tw-rounded-md tw-border tw-border-blue-200 hover:tw-bg-blue-100 tw-transition-colors tw-duration-200">
-                      <input
-                        type="radio"
-                        name="situation"
-                        value={option.id}
-                        className="tw-mt-0.5 tw-h-4 tw-w-4 tw-text-black-600 tw-border-blue-300 focus:tw-ring-blue-500"
-                      />
-                      <div className="tw-ml-3">
-                        <div className="tw-text-base tw-font-medium tw-text-black-800">
-                          {option.label}
-                        </div>
-                        <div className="tw-text-sm tw-text-black-600">
-                          {option.description}
-                        </div>
-                      </div>
-                    </label>
-                  </div>
-                ))}
+                {situationOptions.map((option) => {
+                  const checked = option.id === checkedSituation;
+                  return (
+<ToggleButton
+  type="radio"
+  checked={checked}
+  key={option.id}
+  value={option.id}
+  id={option.id}
+  name="situation"
+  onChange={(e) => { setCheckedSituation(e.target.value as UserSituation); }}
+  className={`toggle-button tw-border-2 tw-min-h-[260px] tw-min-w-56 tw-flex tw-flex-col tw-place-content-center tw-rounded-lg tw-shadow-[0px_0px_8px_4px_rgba(0,0,0,0.15)] 
+                      ${checked ? '!tw-bg-indigo-100 !tw-border-gray-500 !tw-shadow-[0px_0px_8px_4px_rgba(0,0,0,0.4)]' : ''}`}
+>
+                    <img alt={option.iconAlt} src={option.iconSrc} className="tw-my-4 tw-h-24 tw-w-auto tw-aspect-auto tw-pointer-events-none" />
+                    <p className={
+                      `tw-text-lg tw-font-bold tw-mb-2 !active:tw-text-black tw-pointer-events-none tw-text-black
+                      ${checked ? '' : ''}`}
+                    >{option.label}
+                    </p>
+</ToggleButton>
+                  );
+                },
+                  // <div key={option.id} className="tw-relative">
+                  //   <label className="tw-flex tw-items-start tw-cursor-pointer tw-p-3 tw-rounded-md tw-border tw-border-blue-200 hover:tw-bg-blue-100 tw-transition-colors tw-duration-200">
+                  //     <input
+                  //       type="radio"
+                  //       name="situation"
+                  //       value={option.id}
+                  //       className="tw-mt-0.5 tw-h-4 tw-w-4 tw-text-black-600 tw-border-blue-300 focus:tw-ring-blue-500"
+                  //     />
+                  //     <div className="tw-ml-3">
+                  //       <div className="tw-text-base tw-font-medium tw-text-black-800">
+                  //         {option.label}
+                  //       </div>
+                  //       <div className="tw-text-sm tw-text-black-600">
+                  //         {option.description}
+                  //       </div>
+                  //     </div>
+                  //   </label>
+                  // </div>
+                )}
               </div>
               <button
                 type="button"
@@ -201,7 +237,7 @@ const QuickStartCard = () => {
                 disabled={!userSituation}
                 className="tw-inline-flex tw-items-center tw-px-3 tw-py-1 tw-text-base tw-font-medium tw-text-black tw-bg-white tw-border tw-border-black tw-rounded-md hover:tw-bg-[#4b4f9a] focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2 focus:tw-ring-blue-500 disabled:tw-opacity-50 disabled:tw-cursor-not-allowed tw-transition-all tw-duration-200 hover:tw-text-white"
               >
-                Create My Checklist
+                Next
               </button>
             </div>
           </div>
@@ -293,12 +329,20 @@ const QuickStartCard = () => {
             </ul>
 
             {progressPercentage === 100 && (
-              <div className="tw-mt-4 tw-p-3 tw-bg-green-100 tw-border tw-border-green-200 tw-rounded-md tw-transition-all tw-duration-300 tw-ease-in-out">
+              <div className="tw-mt-4 tw-mb-4 tw-p-3 tw-bg-green-100 tw-border tw-border-green-200 tw-rounded-md tw-transition-all tw-duration-300 tw-ease-in-out">
                 <p className="tw-text-base tw-text-green-700 tw-font-medium">
                   🎉 Congratulations! You have completed all quick start tasks.
                 </p>
               </div>
             )}
+
+            <button
+              type="button"
+              onClick={handleBack}
+              className="tw-inline-flex tw-items-center tw-px-3 tw-py-1 tw-text-base tw-font-medium tw-text-black tw-bg-white tw-border tw-border-black tw-rounded-md hover:tw-bg-[#4b4f9a] focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2 focus:tw-ring-blue-500 disabled:tw-opacity-50 disabled:tw-cursor-not-allowed tw-transition-all tw-duration-200 hover:tw-text-white tw-mt-5"
+            >
+              Back
+            </button>
           </div>
         </div>
       </div>
