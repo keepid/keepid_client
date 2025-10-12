e37151833ae288bab7f7877e325e9f7b13fa7e4f
David Fu <fu_david@outlook.com>
2025-10-11 22:52:01 -0400

    Made changes to properly display activities

--- src/components/BaseComponents/BaseActivityCard.tsx
+++ src/components/BaseComponents/BaseActivityCard.tsx
@@ -4,31 +4,44 @@ interface ActivityProps {
   activity: any;
 }
 
-export default function (props: ActivityProps): React.ReactElement {
-  const { activity } = props;
-  const uploaderUsername = activity.username;
+export default function ActivityCard({ activity }: ActivityProps): React.ReactElement {
+  const uploaderUsername = activity.invokerUsername;
+  const objectName = activity.objectName;
   const { type } = activity;
-  if (uploaderUsername !== '' && type !== '') {
-    const displayType = type.split('Activity');
+
+  if (uploaderUsername && type) {
+    const displayType = type.replace('Activity', '');
     const newDate = new Date(Date.parse(activity.occurredAt));
-    // return mm/dd/yyyy version of date
     const dateString = newDate.toLocaleDateString();
-    // return difference number of days between current date and dateString for activity
     const daysDifference = Math.round(
       (new Date().getTime() - newDate.getTime()) / (1000 * 3600 * 24),
     );
-    // eslint-disable-next-line no-underscore-dangle
+
     return (
-      <div key={activity.id} className="ml-2 activities-card-container">
-        <h6 id="activities-card-title">
-          {displayType}
-          Activity
-        </h6>
-        <p id="activities-card-date">
-          {`Completed by ${uploaderUsername}, ${dateString}, ${daysDifference} days ago`}
-        </p>
+      <div
+        key={activity.id}
+        className="activities-card-container flex flex-row justify-between items-center bg-gray-50 rounded-md p-3"
+        style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} // forces flex row
+      >
+        {/* Column 1: Activity type */}
+        <div style={{ flex: '1', textAlign: 'left' }}>
+          <h6 className="text-gray-600 font-semibold">{displayType}Activity</h6>
+        </div>
+
+        {/* Column 2: Object name */}
+        <div style={{ flex: '2', textAlign: 'center' }}>
+          <h6 className="text-gray-800 truncate">{objectName}</h6>
+        </div>
+
+        {/* Column 3: Metadata */}
+        <div style={{ flex: '2', textAlign: 'right' }}>
+          <p className="text-gray-500 text-sm">
+            Completed by {uploaderUsername}, {dateString}, {daysDifference} days ago
+          </p>
+        </div>
       </div>
     );
   }
+
   return <div />;
 }
