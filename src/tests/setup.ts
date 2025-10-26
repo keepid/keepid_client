f2d3451ff191207877de7170397e743240cea68f
Daniel Joo <dsj2122@columbia.edu>
2025-10-26 16:11:23 -0700

    added mock resizeObserver to make tests accept react-slick

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -3,3 +3,11 @@ import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 
 globalThis.jest = vi;
+
+const mockResizeObserver = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+vi.stubGlobal('ResizeObserver', mockResizeObserver);
