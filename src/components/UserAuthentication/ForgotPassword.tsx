4ce1a4cc8ba775909d24c02aca32788e8e0044bb
Daniel Joo <dsj2122@columbia.edu>
2026-02-10 20:28:59 -0500

    horizontally centered password reset pages

--- src/components/UserAuthentication/ForgotPassword.tsx
+++ src/components/UserAuthentication/ForgotPassword.tsx
@@ -65,6 +65,11 @@ class ForgotPassword extends Component<Props, State> {
               + 'Please contact your nonprofit to get your password recovery code.',
             );
             this.setState({ buttonState: '' });
+          } else if (status === 'UNABLE_TO_SEND') {
+            this.props.alert.show(
+              'Unable to send password reset email right now. Please try again later or contact support.',
+            );
+            this.setState({ buttonState: '' });
           } else {
             this.props.alert.show('Server Failure: Please Try Again');
             this.setState({ buttonState: '' });
@@ -85,20 +90,29 @@ class ForgotPassword extends Component<Props, State> {
           <title>Password Recovery</title>
           <meta name="description" content="Keep.id" />
         </Helmet>
-        <div className="container">
-          <div className="row mt-5">
-            <div className="col-4 tw-hidden md:tw-block">
-              <div className="float-right w-100">
-                <img
-                  alt="Password Recovery"
-                  className="w-75 pt-5 mt-5 float-right "
-                  src={ForgotPasswordSVG}
-                />
-              </div>
+        <div className="tw-container tw-mx-auto tw-max-w-5xl tw-px-4">
+          <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-12 tw-py-12 lg:tw-py-20 tw-min-h-[70vh] tw-items-center">
+            <div className="tw-hidden md:tw-flex tw-flex-col tw-items-center tw-justify-center">
+              <img
+                alt="Password Recovery"
+                className="tw-w-4/5 tw-max-w-md"
+                src={ForgotPasswordSVG}
+              />
+              <p className="tw-text-gray-500 tw-text-sm tw-mt-6 tw-text-center tw-max-w-sm">
+                This page is protected by reCAPTCHA, and subject to the Google{' '}
+                <a href="https://www.google.com/policies/privacy/">
+                  Privacy Policy{' '}
+                </a>
+                and{' '}
+                <a href="https://www.google.com/policies/terms/">
+                  Terms of service
+                </a>
+                .
+              </p>
             </div>
 
-            <div className="col-8">
-              <form className="form-signin pt-5 ml-5">
+            <div className="tw-flex tw-justify-center">
+              <form className="form-signin pt-2">
                 <h1 className="h3 mb-3 font-weight-normal">
                   Forgot Your Password?
                 </h1>
@@ -123,34 +137,16 @@ class ForgotPassword extends Component<Props, State> {
                   />
                 </label>
 
-                <div className="row pt-3">
-                  <div className="col-6">
-                    <Button
-                      type="submit"
-                      onClick={this.handlePasswordReset}
-                      className={`w-100 ld-ext-right ${this.state.buttonState}`}
-                      variant="primary"
-                    >
-                      Submit
-                      <div className="ld ld-ring ld-spin" />
-                    </Button>
-                  </div>
-                </div>
-
-                <div className="row pl-3 pb-3">
-                  <span className="text-muted recaptcha-login-text mt-4">
-                    This page is protected by reCAPTCHA, and subject to the
-                    Google
-                    <a href="https://www.google.com/policies/privacy/">
-                      Privacy Policy
-{' '}
-                    </a>
-                    and
-                    <a href="https://www.google.com/policies/terms/">
-                      Terms of service
-                    </a>
-                    .
-                  </span>
+                <div className="pt-3">
+                  <Button
+                    type="submit"
+                    onClick={this.handlePasswordReset}
+                    className={`px-5 ld-ext-right ${this.state.buttonState}`}
+                    variant="primary"
+                  >
+                    Submit
+                    <div className="ld ld-ring ld-spin" />
+                  </Button>
                 </div>
               </form>
             </div>
